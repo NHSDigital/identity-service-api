04a35ddd26e0cf489958bbdf83ad58a46cfde1c3
Alex Hawdon <alex.hawdon@airelogic.com>
2020-04-15 16:47:07 +0100

    Referenced module doesn't need to point to a branch

--- terraform/main.tf
+++ terraform/main.tf
@@ -13,7 +13,7 @@ terraform {
 }
 
 module "identity-service" {
-  source             = "github.com/NHSDigital/api-platform-service-module?ref=apm-501-change-proxy-location"
+  source             = "github.com/NHSDigital/api-platform-service-module"
   name               = "identity-service"
   path               = "identity-service"
   apigee_environment = var.apigee_environment
