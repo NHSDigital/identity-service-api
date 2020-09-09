67332d2f9afee45bf0912ccfa899fa2e9b9cad41
Luckman Ali <luckman.ali@live.com>
2020-09-09 13:33:23 +0100

    updated environment and config variables

--- api_tests/config_files/config.py
+++ api_tests/config_files/config.py
@@ -7,6 +7,16 @@ BASE_URL = ENV['oauth']['base_url']
 AUTHORIZE_URL = f"{BASE_URL}/authorize"
 TOKEN_URL = f"{BASE_URL}/token"
 
+# Apigee API details
+APIGEE_API_URL = ENV['apigee']['base_url']
+APIGEE_AUTHENTICATION = ENV['apigee']['api_authentication']
+APIGEE_ENVIRONMENT = "internal-dev"
+
+# PDS
+PDS_BASE_URL = ENV['pds']['base_url']
+PDS_APPLICATION = ENV['pds']['application_name']
+PDS_API = f"{PDS_BASE_URL}/{PDS_APPLICATION}/Patient"
+
 # App details
 CLIENT_ID = ENV['oauth']['client_id']
 CLIENT_SECRET = ENV['oauth']['client_secret']
@@ -19,4 +29,13 @@ USERNAME = ENV['oauth']['authenticate_username']
 PASSWORD = ENV['oauth']['authenticate_password']
 
 # Test API
-API_URL = ENV['oauth']['api_url']
+API_URL = ENV['hello_world']['api_url']
+
+# Endpoints
+ENDPOINTS = {
+    'authorize': AUTHORIZE_URL,
+    'token': TOKEN_URL,
+    'authenticate': AUTHENTICATE_URL,
+    'api': API_URL,
+    'pds': PDS_API,
+}
--- api_tests/config_files/environments.py
+++ api_tests/config_files/environments.py
@@ -12,6 +12,8 @@ def get_env(variable_name: str, default: str = "") -> str:
 
 ENV = {
     'oauth': {
+        'apigee_client_id': get_env('APIGEE_CLIENT_ID'),
+
         'base_url': get_env('BASE_URL'),
         'client_id': get_env('CLIENT_ID'),
         'client_secret': get_env('CLIENT_SECRET'),
@@ -22,7 +24,20 @@ ENV = {
         'authenticate_username': get_env('AUTHENTICATE_USERNAME'),
         'authenticate_password': get_env('AUTHENTICATE_PASSWORD'),
 
-        'apigee_client_id': get_env('APIGEE_CLIENT_ID'),
+        # Invalid ASID Application details
+        'invalid_asic_client_id': get_env('INVALID_ASID_CLIENT_ID'),
+        'invalid_asid_client_secret': get_env('INVALID_ASID_CLIENT_SECRET'),
+        'invalid_asid_redirect_uri': get_env('INVALID_ASID_REDIRECT_URI'),
+    },
+    'apigee': {
+        'base_url': get_env('APIGEE_API_URL'),
+        'api_authentication': get_env('APIGEE_API_AUTHENTICATION'),
+    },
+    'hello_world': {
         'api_url': get_env('API_URL'),
     },
+    'pds': {
+        'base_url': get_env('PDS_BASE_URL'),
+        'application_name': get_env('PDS_APPLICATION'),
+    }
 }
