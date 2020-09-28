173080601edc4ab588774a88e06e6ace81c06c72
Ben Strutt <ben.strutt1@nhs.net>
2020-09-28 15:49:18 +0100

    APM-991 Use apigee RandomLong() to generate random bits.
    And "concatenate" the random bits together into a nice base64 encoded
    state string, such that we have 4*54=216 bits of entropy.

--- proxies/live/apiproxy/resources/py/generate-state.py
+++ proxies/live/apiproxy/resources/py/generate-state.py
@@ -1,15 +1,43 @@
-import os
-import urllib
+import struct
 import base64
 
-# 32 bytes = 256 bits, well above the RFC recommended 160.
-# https://tools.ietf.org/html/rfc6749#section-10.10
-random_bytes = os.urandom(32)
+NUM_RANDOM_LONG_INTS = {{ NUM_RANDOM_LONG_INTS_FOR_STATE }}
 
-# reduce length of token w/ base64 encoding
-random_bytes_str = base64.b64encode(random_bytes)
+# Ancient python 2.5 has no handy string-to-binary representation, so
+# we convert to a hex string and finish the job ourselves.
+HEX_TO_BIN = {
+    '0': '0000', '1': '0001', '2': '0010', '3': '0011',
+    '4': '0100', '5': '0101', '6': '0110', '7': '0111',
+    '8': '1000', '9': '1001', 'a': '1010', 'b': '1011',
+    'c': '1100', 'd': '1101', 'e': '1110', 'f': '1111'
+}
 
-# Make safe to pass around as a query parameter
-safe_random_bytes_str = urllib.quote(random_bytes_str, safe='')
 
-flow.setVariable('apigee.state', safe_random_bytes_str)
+def int_to_bin_str(int_):
+    # Pad to 64 bits in case leading 4 bits are 0.
+    return "".join(HEX_TO_BIN[v] for v in "%x" % int_).zfill(64)
+
+
+# Grab Apigee's randomLong() generated values.
+random_ints = [
+    int(flow.getVariable("private.randomLong%d" % i))
+    for i in xrange(NUM_RANDOM_LONG_INTS)
+]
+
+# Some experimentation reveals that Apigee's randomLong() returns a 64
+# bit random number with only the first 54 bits containing random
+# data.
+random_very_long_binary_str = "".join(int_to_bin_str(int_)[:54] for int_ in random_ints)
+
+# Convert to base64 for shorter, more human readable output by shoving
+# actual bytes into a string (since this is python 2.5, we need to use
+# struct.pack instead of bytearray).
+random_very_long_bytes = "".join(
+    struct.pack("B", int(random_very_long_binary_str[i:i+8], 2))
+    for i in xrange(0, len(random_very_long_binary_str), 8)
+)
+random_very_long_b64 = base64.urlsafe_b64encode(random_very_long_bytes)
+
+# Finally, 54*4 = 216 bits of randomness in a mere 36 url-safe, ascii
+# characters.
+flow.setVariable("private.secure_state", random_very_long_b64)
