3bb9faf6c72e01c4f6971788a059ab07e37b6cda
Luckman Ali <luckman.ali@live.com>
2020-09-28 13:09:19 +0100

    updated authorize error condition tests

--- api_tests/tests/oauth/test_oauth_endpoints.py
+++ api_tests/tests/oauth/test_oauth_endpoints.py
@@ -83,47 +83,13 @@ class TestOauthEndpointSuite:
         },
 
         # condition 2: missing redirect uri
-        # {
-        #     'expected_status_code': 400,
-        #     'expected_response': {
-        #         'error': 'invalid_request',
-        #         'error_description': 'redirect_uri is missing'
-        #     },
-        #     'params': {
-        #         'client_id': config.CLIENT_ID,
-        #         'response_type': 'code',
-        #         'state': random.getrandbits(32)
-        #     },
-        # },
+
 
         # condition 3: invalid client id
-        # {
-        #     'expected_status_code': 401,
-        #     'expected_response': {
-        #         'error': 'invalid_request',
-        #         'error_description': "client_id is invalid"
-        #     },
-        #     'params': {
-        #         'client_id': 'THISisANinvalidCLIENTid12345678',  # invalid client id
-        #         'redirect_uri': config.REDIRECT_URI,
-        #         'response_type': 'code',
-        #         'state': random.getrandbits(32)
-        #     },
-        # },
+
 
         # condition 4: missing client id
-        # {
-        #     'expected_status_code': 400,
-        #     'expected_response': {
-        #         'error': 'invalid_request',
-        #         'error_description': 'client_id is missing'
-        #     },
-        #     'params': {
-        #         'redirect_uri': config.REDIRECT_URI,
-        #         'response_type': 'code',
-        #         'state': random.getrandbits(32)
-        #     },
-        # },
+
 
         # condition 5: invalid response type
         {
@@ -141,18 +107,6 @@ class TestOauthEndpointSuite:
         },
 
         # condition 6: missing response type
-        {
-            'expected_status_code': 400,
-            'expected_response': {
-                'ErrorCode': 'invalid_request',
-                'Error': 'response_type is missing'
-            },
-            'params': {
-                'client_id': config.CLIENT_ID,
-                'redirect_uri': config.REDIRECT_URI,
-                'state': random.getrandbits(32)
-            },
-        }
     ])
     def test_authorization_error_conditions(self, request_data: dict):
         assert self.oauth.check_endpoint('GET', 'authorize', **request_data)
