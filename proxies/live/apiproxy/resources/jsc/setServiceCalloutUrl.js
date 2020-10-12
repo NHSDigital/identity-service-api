c5b50b4c9df28ca580734ee70151c3e66f9b48e5
Alex Hawdon <alex.hawdon@airelogic.com>
2020-10-12 20:50:43 +0100

    bugfix

--- proxies/live/apiproxy/resources/jsc/setServiceCalloutUrl.js
+++ proxies/live/apiproxy/resources/jsc/setServiceCalloutUrl.js
@@ -2,6 +2,6 @@ var urlvalue = context.getVariable('verifyapikey.VerifyAPIKey.FromJWT.jwks-resou
 var re = new RegExp('^(https?:\/\/[^/]+)(\/.*)$');
 var match = re.exec(urlvalue);
 if (match) {
-  context.setVariable('servicecallout.ServiceCallout.GetJWKS.target.url', match[1]);
+  context.setVariable('servicecallout.ServiceCallout.ClientCredentialsGrantGetJWKS.target.url', match[1]);
   context.setVariable('sc_urlPath', match[2]);
 }
