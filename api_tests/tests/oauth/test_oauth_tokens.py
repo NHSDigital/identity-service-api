3b67b653b5f230f6009f4cd87e50e1630dd6e450
Henry Iwanejko <henry.iwanejko1@nhs.net>
2020-10-21 11:24:34 +0100

    APM-1335 Change override conditional

--- api_tests/tests/oauth/test_oauth_tokens.py
+++ api_tests/tests/oauth/test_oauth_tokens.py
@@ -125,7 +125,7 @@ class TestOauthTokenSuite:
     @pytest.mark.apm_1010
     @pytest.mark.errors
     @pytest.mark.usefixtures('get_refresh_token')
-    @pytest.mark.skip(reason="There is a bug raised for this: APM-1335")
+    # @pytest.mark.skip(reason="There is a bug raised for this: APM-1335")
     def test_refresh_token_does_expire(self):
         sleep(5)
         assert self.oauth.check_endpoint(
