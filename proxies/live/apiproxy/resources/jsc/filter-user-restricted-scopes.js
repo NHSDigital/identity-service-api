cd2c5dc644273134f4a956af22df37a6ba5f30e5
Henry Iwanejko <henry.iwanejko1@nhs.net>
2021-01-28 12:07:42 +0000

    APM-1701 Try resolve complimation error

--- proxies/live/apiproxy/resources/jsc/filter-user-restricted-scopes.js
+++ proxies/live/apiproxy/resources/jsc/filter-user-restricted-scopes.js
@@ -4,8 +4,8 @@ var scopes_list = api_product_scopes.split(" ");
 let regex = new RegExp("(urn:nhsd:apim:user:aal3:*)");
 var filtered_user_restricted_scopes = scopes_list.filter(scope => {
     if (regex.test(scope)) {
-        return scope
-    };
+        return scope;
+    }
 });
 
 context.setVariable('apigee.user_restricted_scopes', filtered_user_restricted_scopes);
