db0d64b9ddac8fe824fdc93260294a887ff9fe94
Valentinolucas <valentinolucasfantini@gmail.com>
2021-03-03 14:18:31 +0000

    AMB-520 Add javascript policy to check subject_token

---
+++ proxies/live/apiproxy/resources/jsc/validate-claim.js
@@ -0,0 +1,22 @@
+id_token = context.getVariable('request.formparam.subject_token');
+print(id_token);
+
+
+// if (context.getVariable('identity_service.simulated_idp') == 'true') {
+//   state = context.getVariable('request.queryparam.state') || "";
+// } else {
+//   state = context.getVariable('request.queryparam.state');
+// }
+// redirect_uri = context.getVariable('request.queryparam.redirect_uri');
+// response_type = context.getVariable('request.queryparam.response_type');
+// scope = context.getVariable('apigee.user_restricted_scopes');
+
+// var cacheEntry = {
+//   client_id: client_id,
+//   redirect_uri: redirect_uri,
+//   state: state,
+//   response_type: response_type,
+//   scope: scope
+// };
+
+// context.setVariable('cacheEntry', JSON.stringify(cacheEntry));
