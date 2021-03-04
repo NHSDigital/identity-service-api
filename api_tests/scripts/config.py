03ca33eeb6a45d3eff3c42dc82d3337183c82d57
Adrian Clare <adrian.clare@airelogic.com>
2021-03-04 09:41:48 +0000

    modifed config

--- api_tests/scripts/config.py
+++ api_tests/scripts/config.py
@@ -1,6 +1,20 @@
 from os import environ
 import os
 
+def get_env_file(variable_name: str) -> str:
+    """Returns a environment variable as path"""
+    try:
+        path = os.path.abspath(os.environ[variable_name])
+        if not path:
+            raise RuntimeError(f"Variable is null, Check {variable_name}.")
+        with open(path, "r") as f:
+            contents = f.read()
+        if not contents:
+            raise RuntimeError(f"Contents of file empty. Check {variable_name}.")
+        return contents
+    except KeyError:
+        raise RuntimeError(f"Variable is not set, Check {variable_name}.")
+
 # Oauth details
 OAUTH_BASE_URI = environ["OAUTH_BASE_URI"]
 OAUTH_PROXY = environ["OAUTH_PROXY"]
