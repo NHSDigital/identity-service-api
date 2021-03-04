2fca9ea57ecdb4c90f9f271e103626bff8f9055d
ryan.thomas5 <ryan_j_thomas@hotmail.co.uk>
2021-03-04 10:48:24 +0000

    AMB-520 add semni colons to js policy

--- proxies/live/apiproxy/resources/jsc/validate-claim.js
+++ proxies/live/apiproxy/resources/jsc/validate-claim.js
@@ -9,9 +9,9 @@ token_updated='false';
 
 for (let i = 0; i < jwksObj.keys.length; i++) {
   if (kid = jwksObj.keys[i].kid) {
-    return token_updated='true'
-  }
-}
+    return token_updated='true';
+  };
+};
 
 
 context.setVariable('token_updated', token_updated);
