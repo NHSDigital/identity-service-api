093ae1cd4b6d932c327fd0794fa17af78883788e
Valentinolucas <valentinolucasfantini@gmail.com>
2021-03-05 08:58:58 +0000

    AMB-520 refactor validate-claim javascript

--- proxies/live/apiproxy/resources/jsc/validate-claim.js
+++ proxies/live/apiproxy/resources/jsc/validate-claim.js
@@ -2,17 +2,12 @@ jwksString = context.getVariable('jwks');
 jwksObj = JSON.parse(jwksString);
 kid = context.getVariable('jwt.DecodeJWT.FromSubjectTokenFormParam.decoded.header.kid');
 
-print(jwksObj.keys[0].kid);
-print(kid);
-
 token_updated='false';
 
 for (var i = 0; i < jwksObj.keys.length; i++) {
   if (kid == jwksObj.keys[i].kid) {
       token_updated='true';
-    //   return token_updated;
   }
 }
-
 
 context.setVariable('token_updated', token_updated);
