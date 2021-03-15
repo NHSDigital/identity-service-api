eb6989d532e20b3c206effb98e8555ccd4f37dc9
RyanThomas1214 <ryan_j_thomas@hotmail.co.uk>
2021-03-15 13:36:07 +0000

    AMB-520 rename js variable + use booleans

--- proxies/live/apiproxy/resources/jsc/validate-claim-external-id-token.js
+++ proxies/live/apiproxy/resources/jsc/validate-claim-external-id-token.js
@@ -2,12 +2,12 @@ jwksString = context.getVariable('jwks');
 jwksObj = JSON.parse(jwksString);
 kid = context.getVariable('jwt.DecodeJWT.FromExternalIdToken.decoded.header.kid');
 
-token_updated='false';
+jwks_updated = false;
 
 for (var i = 0; i < jwksObj.keys.length; i++) {
   if (kid == jwksObj.keys[i].kid) {
-      token_updated='true';
+      jwks_updated = true;
   }
 }
 
-context.setVariable('token_updated', token_updated);
+context.setVariable('jwks_updated', jwks_updated);
