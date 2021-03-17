23736e6971c8fb56fca865c47e0d4bb554784339
Matthew Clark <matthew.clark21@nhs.net>
2021-03-17 17:00:40 +0000

    AMB-519 Added audience claim validation to Javascript polocy + raise fault

--- proxies/live/apiproxy/resources/jsc/validate-id-token-claims.js
+++ proxies/live/apiproxy/resources/jsc/validate-id-token-claims.js
@@ -1,2 +1,26 @@
-var target_url = context.getVariable("servicecallout.ServiceCallout.PostToken.target.url");
-print(target_url);
+var aud_claim = context.getVariable("jwt.DecodeJWT.FromExternalIdToken.decoded.claim.aud");
+var client_id = context.getVariable("private.apigee.client_id");
+var message_id = context.getVariable("messageid");
+
+aud_claim = JSON.parse(aud_claim);
+validation = {
+              message_id: message_id,
+              is_valid: true,
+              message : ""    
+            };
+            
+            
+if(aud_claim == client_id)
+    {
+        message =   
+        {
+          error: "AudClaim Invalid",
+          error_description: "Aud claim does not match",
+          message_id: message_id
+        }
+        validation.is_valid = false;
+        validation.message = JSON.stringify(message);
+    }
+    
+context.setVariable("claims_validation.is_valid", validation.is_valid);    
+context.setVariable("claims_validation.message", validation.message);
