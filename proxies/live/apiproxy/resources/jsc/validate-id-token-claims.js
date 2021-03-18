80df735668f7b38fc4a305473845ff2e6d4f3a34
Matthew Clark <matthew.clark21@nhs.net>
2021-03-18 10:51:32 +0000

    AMB-519 Added issue claim comparison

--- proxies/live/apiproxy/resources/jsc/validate-id-token-claims.js
+++ proxies/live/apiproxy/resources/jsc/validate-id-token-claims.js
@@ -1,4 +1,7 @@
 var aud_claim = context.getVariable("jwt.DecodeJWT.FromExternalIdToken.decoded.claim.aud");
+var iss_claim = context.getVariable("jwt.DecodeJWT.FromExternalIdToken.decoded.claim.iss");
+var authorize_endpoint = context.getVariable("private.apigee.authorize_endpoint");
+
 var client_id = context.getVariable("private.apigee.client_id");
 var message_id = context.getVariable("messageid");
 
@@ -9,7 +12,10 @@ validation = {
               message : ""    
             };
             
-            
+var index = authorize_endpoint.indexOf('/authorize')
+var base_url = authorize_endpoint.slice(0, index)
+
+
 if(aud_claim != client_id)
     {
         message =   
@@ -22,5 +28,16 @@ if(aud_claim != client_id)
         validation.message = JSON.stringify(message);
     }
     
+else if(iss_claim.toLowerCase() != base_url.toLowerCase())
+    {
+        message =   
+        {
+          error: "IssClaim Invalid",
+          error_description: "Iss claim does not match",
+          message_id: message_id
+        }
+        validation.is_valid = false;
+        validation.message = JSON.stringify(message);
+    }
 context.setVariable("claims_validation.is_valid", validation.is_valid);    
 context.setVariable("claims_validation.message", validation.message);
