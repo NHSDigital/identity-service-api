573e16bd2ebaa8b15543665996f7445449273494
ryan.thomas5 <ryan_j_thomas@hotmail.co.uk>
2021-03-19 11:23:42 +0000

    AMB-519 refactor JS policy

--- proxies/live/apiproxy/resources/jsc/validate-id-token-claims.js
+++ proxies/live/apiproxy/resources/jsc/validate-id-token-claims.js
@@ -3,14 +3,8 @@ var iss_claim = context.getVariable("jwt.DecodeJWT.FromExternalIdToken.decoded.c
 var authorize_endpoint = context.getVariable("private.apigee.authorize_endpoint");
 
 var client_id = context.getVariable("private.apigee.client_id");
-var message_id = context.getVariable("messageid");
 
 aud_claim = JSON.parse(aud_claim);
-validation = {
-              message_id: message_id,
-              is_valid: true,
-              message : ""    
-            };
             
 var index = authorize_endpoint.indexOf('/authorize')
 var base_url = authorize_endpoint.slice(0, index)
@@ -18,26 +12,14 @@ var base_url = authorize_endpoint.slice(0, index)
 
 if(aud_claim != client_id)
     {
-        message =   
-        {
-          error: "invalid_request",
-          error_description: "Invalid aud claim in JWT",
-          message_id: message_id
-        }
-        validation.is_valid = false;
-        validation.message = JSON.stringify(message);
+        context.setVariable('claims_validation.error', "invalid_request")
+        context.setVariable('claims_validation.error_description', "Invalid aud claim in JWT")
+        context.setVariable('claims_validation.is_valid', false)
     }
     
 else if(iss_claim.toLowerCase() != base_url.toLowerCase())
     {
-        message =   
-        {
-          error: "invalid_request",
-          error_description: "Invalid iss claim in JWT",
-          message_id: message_id
-        }
-        validation.is_valid = false;
-        validation.message = JSON.stringify(message);
+      context.setVariable('claims_validation.error', "invalid_request")
+      context.setVariable('claims_validation.error_description', "Invalid iss claim in JWT")
+      context.setVariable('claims_validation.is_valid', false)
     }
-context.setVariable("claims_validation.is_valid", validation.is_valid);    
-context.setVariable("claims_validation.message", validation.message);
