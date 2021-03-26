0fc109b733d3ba8b5850e3fe2271b829c584324f
Valentinolucas <valentinolucasfantini@gmail.com>
2021-03-26 15:20:51 +0000

    AMB-605 Added new caching for token exchange

--- proxies/live/apiproxy/resources/jsc/SelectIdTokenIssuer.js
+++ proxies/live/apiproxy/resources/jsc/SelectIdTokenIssuer.js
@@ -8,12 +8,16 @@ var idTokenIssuer = ''
 if (matchNhsCis2) {
     print('this is nhs id');
     idTokenIssuer = 'nhsCis2'; 
+    jwksPath = '/mock-nhsid-jwks/identity-service/nhs-cis2-jwks'
 } else if (matchNhsLogin) {
     print('this is nhs login');
     idTokenIssuer = 'nhsLogin';
+    jwksPath = '/mock-nhsid-jwks/identity-service/nhs-login-jwks'
+
 } else {
     print('error');
     idTokenIssuer = ''
 }
 
 context.setVariable('idTokenIssuer', idTokenIssuer);
+context.setVariable('jwksPath', jwksPath);
