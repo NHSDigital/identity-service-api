4f1991b3c7f10ef55415e7ea22e966610cb8cefe
Valentinolucas <valentinolucasfantini@gmail.com>
2021-03-26 12:14:38 +0000

    AMB-605 Add idTokenIssuer context variable in javascript policy

--- proxies/live/apiproxy/resources/jsc/SelectIdTokenIssuer.js
+++ proxies/live/apiproxy/resources/jsc/SelectIdTokenIssuer.js
@@ -3,11 +3,17 @@ var nhsCis2 = new RegExp("(NHSIdentity)");
 var nhsLogin = new RegExp("(signin.nhs.uk)");
 var matchNhsCis2 = nhsCis2.exec(issuer);
 var matchNhsLogin = nhsLogin.exec(issuer);
+var idTokenIssuer = ''
 
 if (matchNhsCis2) {
-    print('this is nhs id')
+    print('this is nhs id');
+    idTokenIssuer = 'nhsCis2'; 
 } else if (matchNhsLogin) {
-    print('this is nhs login')
+    print('this is nhs login');
+    idTokenIssuer = 'nhsLogin';
 } else {
-    print('error')
+    print('error');
+    idTokenIssuer = ''
 }
+
+context.setVariable('idTokenIssuer', idTokenIssuer);
