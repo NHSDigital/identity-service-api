4adfe6801c1149f600e70146bd7d10fb42254015
Valentinolucas <valentinolucasfantini@gmail.com>
2021-03-30 12:15:54 +0100

    AMB-605 Consume kvm vars in javascript for service callout

--- proxies/live/apiproxy/resources/jsc/SelectIdTokenIssuer.js
+++ proxies/live/apiproxy/resources/jsc/SelectIdTokenIssuer.js
@@ -1,4 +1,3 @@
-var identityServiceConfigVars = context.getVariable('identity-service-config')
 var issuer = context.getVariable('jwt.DecodeJWT.FromSubjectTokenFormParam.decoded.claim.iss');
 var nhsCis2 = new RegExp("(NHSIdentity)");
 var nhsLogin = new RegExp("(signin.nhs.uk)");
@@ -6,12 +5,13 @@ var matchNhsCis2 = nhsCis2.exec(issuer);
 var matchNhsLogin = nhsLogin.exec(issuer);
 var idTokenIssuer = ''
 
+
 if (matchNhsCis2) {
     idTokenIssuer = 'nhsCis2'; 
-    jwksPath = identityServiceConfigVars.cis2.jwks_path
+    jwksPath = context.getVariable('identity-service-config.cis2.jwks_path');
 } else if (matchNhsLogin) {
     idTokenIssuer = 'nhsLogin';
-    jwksPath = identityServiceConfigVars.nhs-login.jwks_path
+    jwksPath = context.getVariable('identity-service-config.nhs_login.jwks_path');
 
 } else {
     print('error');
