3b478ed84f9fe22b9479d02fdcc214c82aacbdf6
RyanThomas1214 <ryan_j_thomas@hotmail.co.uk>
2021-03-31 11:53:43 +0100

    AMB-605 refactor JS policy + include prod iss value

--- proxies/live/apiproxy/resources/jsc/SelectIdTokenIssuer.js
+++ proxies/live/apiproxy/resources/jsc/SelectIdTokenIssuer.js
@@ -1,18 +1,13 @@
 var issuer = context.getVariable('jwt.DecodeJWT.FromSubjectTokenFormParam.decoded.claim.iss');
-var nhsCis2 = new RegExp("(NHSIdentity)");
-var nhsLogin = new RegExp("(signin.nhs.uk)");
-var matchNhsCis2 = nhsCis2.exec(issuer);
-var matchNhsLogin = nhsLogin.exec(issuer);
 var idTokenIssuer = ''
 
 
-if (matchNhsCis2) {
+if (issuer.includes('NHSIdentity')) {
     idTokenIssuer = 'nhsCis2'; 
     jwksPath = context.getVariable('identity-service-config.cis2.jwks_path');
-} else if (matchNhsLogin) {
+} else if (issuer.includes('signin.nhs.uk') || issuer.includes('login.nhs.uk')) {
     idTokenIssuer = 'nhsLogin';
     jwksPath = context.getVariable('identity-service-config.nhs_login.jwks_path');
-
 } else {
     print('error');
     idTokenIssuer = ''
