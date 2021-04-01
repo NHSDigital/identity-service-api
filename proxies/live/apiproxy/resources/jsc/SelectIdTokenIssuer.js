364f8a842e78c3dfed5803a06edcbbbfb97ce10a
Valentinolucas <valentinolucasfantini@gmail.com>
2021-04-01 14:29:59 +0100

    AMB-605 move javascript policy before error faults

--- proxies/live/apiproxy/resources/jsc/SelectIdTokenIssuer.js
+++ proxies/live/apiproxy/resources/jsc/SelectIdTokenIssuer.js
@@ -1,17 +1,19 @@
 var issuer = context.getVariable('jwt.DecodeJWT.FromSubjectTokenFormParam.decoded.claim.iss');
 var idTokenIssuer = ''
+var InvalidIdTokenIssuer = false
 
-
-if (issuer.includes('NHSIdentity')) {
+if(issuer = null || issuer == '' ) {
+    InvalidIdTokenIssuer = true;
+} else if (issuer.includes('NHSIdentity')) {
     idTokenIssuer = 'nhsCis2'; 
     jwksPath = context.getVariable('identity-service-config.cis2.jwks_path');
 } else if (issuer.includes('signin.nhs.uk') || issuer.includes('login.nhs.uk')) {
     idTokenIssuer = 'nhsLogin';
     jwksPath = context.getVariable('identity-service-config.nhs_login.jwks_path');
 } else {
-    print('error');
-    idTokenIssuer = ''
+    InvalidIdTokenIssuer = true;
 }
 
 context.setVariable('idTokenIssuer', idTokenIssuer);
 context.setVariable('jwksPath', jwksPath);
+context.setVariable('InvalidIdTokenIssuer', InvalidIdTokenIssuer)
