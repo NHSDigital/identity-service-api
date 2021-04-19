fade75f94a4905203963b81bb48220c7147f0a7c
Valentinolucas <valentinolucasfantini@gmail.com>
2021-04-19 10:41:47 +0100

    AMB-673 fix conditions in javascript

--- proxies/live/apiproxy/resources/jsc/SelectIdTokenIssuer.js
+++ proxies/live/apiproxy/resources/jsc/SelectIdTokenIssuer.js
@@ -1,29 +1,32 @@
-var exp = context.getVariable('jwt.DecodeJWT.FromSubjectTokenFormParam.decoded.claim');
+var exp = context.getVariable('jwt.DecodeJWT.FromSubjectTokenFormParam.decoded.claim.exp');
 var iss = context.getVariable('jwt.DecodeJWT.FromSubjectTokenFormParam.decoded.claim.iss');
 var aud = context.getVariable('jwt.DecodeJWT.FromSubjectTokenFormParam.decoded.claim.aud');
 
 var nhsLoginIssuer = context.getVariable('identity-service-config.nhs_login.issuer');
 var cis2Issuer = 'https://am.nhsint.auth-ptl.cis2.spineservices.nhs.uk:443/openam/oauth2/realms/root/realms/NHSIdentity/realms/Healthcare';
 
+
 var jwksPath = '';
 var idTokenIssuer = '';
 var isError = false;
 
-if (exp === "" || exp === null) {
+if (exp === '[""]' || exp === null) {
     var errorObject = { error: 'invalid_request', errorDescription: "Missing exp claim in JWT", statusCode: 400, reasonPhrase: "Bad Request" } 
     var isError = true
-} else if(iss === null || iss === '' ||  iss !== cis2Issuer || iss !== nhsLoginIssuer) {
+} else if(iss === null || iss === '[""]' || iss !== cis2Issuer && iss !== nhsLoginIssuer) {
     var errorObject = { error: 'invalid_request', errorDescription: "Missing or non-matching iss/sub claims in JWT", statusCode: 400, reasonPhrase: "Bad Request" } 
     var isError = true
-} else if (iss == cis2Issuer) {
+} else if(aud === null || aud === '[""]' ) {
+    var errorObject = { error: 'invalid_request', errorDescription: "Missing aud claim in JWT", statusCode: 400, reasonPhrase: "Bad Request" } 
+    var isError = true
+}
+
+if (iss == cis2Issuer) {
     idTokenIssuer = 'nhsCis2'; 
     jwksPath = context.getVariable('identity-service-config.cis2.jwks_path');
 } else if (iss == nhsLoginIssuer) {
     idTokenIssuer = 'nhsLogin';
     jwksPath = context.getVariable('identity-service-config.nhs_login.jwks_path');
-}  else if(aud === null || aud === '' ) {
-    var errorObject = { error: 'invalid_request', errorDescription: "Missing aud claim in JWT", statusCode: 400, reasonPhrase: "Bad Request" } 
-    var isError = true
 }
 
 context.setVariable('isError', isError)
