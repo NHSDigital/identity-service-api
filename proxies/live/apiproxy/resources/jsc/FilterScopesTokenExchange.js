799dc91699ee54c39857b68833745c6a10825a6c
RyanThomas1214 <ryan_j_thomas@hotmail.co.uk>
2021-04-21 17:11:20 +0100

    AMB-573 add condition to JS token exchange filtering for NHS Login scopes

--- proxies/live/apiproxy/resources/jsc/FilterScopesTokenExchange.js
+++ proxies/live/apiproxy/resources/jsc/FilterScopesTokenExchange.js
@@ -1,6 +1,12 @@
 var id_token_acr = context.getVariable('jwt.DecodeJWT.FromSubjectTokenFormParam.decoded.claim.acr');
 var api_product_scopes = String(context.getVariable('original_scope'));
-id_token_acr = ':' + id_token_acr.slice(0, 4).toLowerCase() + ':';
+if (idTokenIssuer == "nhsCis2") {
+    id_token_acr = ':' + id_token_acr.slice(0, 4).toLowerCase() + ':';   
+}
+else {
+    id_token_acr = ':' + id_token_acr.slice(0, 2).toLowerCase() + ':'; 
+}
+
 
 if (api_product_scopes == 'null') {
     api_product_scopes = String(context.getVariable('oauthv2accesstoken.OAuthV2.TokenExchangeGenerateAccessToken.scope'));
