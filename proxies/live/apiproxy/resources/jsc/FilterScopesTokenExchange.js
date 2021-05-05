9f6708c51243cd94ed736b850f3e6aa9a83ae0cd
RyanThomas1214 <ryan_j_thomas@hotmail.co.uk>
2021-05-05 13:13:04 +0100

    AMB-573 add raise fault for missing acr/identity_proofing_level claim

--- proxies/live/apiproxy/resources/jsc/FilterScopesTokenExchange.js
+++ proxies/live/apiproxy/resources/jsc/FilterScopesTokenExchange.js
@@ -1,5 +1,19 @@
 var api_product_scopes = String(context.getVariable('original_scope'));
 var idTokenIssuer = context.getVariable('idTokenIssuer');
+var acr = context.getVariable('jwt.DecodeJWT.FromSubjectTokenFormParam.decoded.claim.acr');
+var identity_proofing_level = context.getVariable('jwt.DecodeJWT.FromSubjectTokenFormParam.decoded.claim.identity_proofing_level');
+var isError = false;
+var missing_claim = '';
+
+if (acr === '') {
+    var isError = true;
+    var missing_claim = 'acr';
+}
+else if (identity_proofing_level === '') {
+    var isError = true;
+    var missing_claim = 'identity_proofing_level';
+}
+
 if (idTokenIssuer == "nhsCis2") {
     var id_token_acr = context.getVariable('jwt.DecodeJWT.FromSubjectTokenFormParam.decoded.claim.acr');
     id_token_acr = ':' + id_token_acr.slice(0, 4).toLowerCase() + ':';   
@@ -23,3 +37,5 @@ var filtered_user_restricted_scopes = scopes_list.filter(scope => {
 });
 filtered_user_restricted_scopes = filtered_user_restricted_scopes.join(' ');
 context.setVariable('apigee.user_restricted_scopes', filtered_user_restricted_scopes);
+context.setVariable('isError', isError);
+context.setVariable('missing_claim', missing_claim);
