08e9577d5cfd335cf344c02bd72c2bbae7dd0e2b
RyanThomas1214 <ryan_j_thomas@hotmail.co.uk>
2021-06-08 11:32:37 +0100

    AMB-722 update clientinfo cache to include IDP

--- proxies/live/apiproxy/resources/jsc/create-cache-entry.js
+++ proxies/live/apiproxy/resources/jsc/create-cache-entry.js
@@ -8,13 +8,15 @@ if (context.getVariable('identity-service-config.cis2.simulated_idp') == 'true')
 redirect_uri = context.getVariable('request.queryparam.redirect_uri');
 response_type = context.getVariable('request.queryparam.response_type');
 scope = context.getVariable('oauthv2accesstoken.OAuthV2.GenerateAccessTokenDummy.scope');
+idp = context.getVariable('request.queryparam.scope');
 
 var cacheEntry = {
   client_id: client_id,
   redirect_uri: redirect_uri,
   state: state,
   response_type: response_type,
-  scope: scope
+  scope: scope,
+  idp: idp
 };
 
 context.setVariable('cacheEntry', JSON.stringify(cacheEntry));
