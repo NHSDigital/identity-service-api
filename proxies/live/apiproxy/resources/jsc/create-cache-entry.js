2dcfd05a521b6141d9e914d03ed42a6dd97d9323
RyanThomas1214 <ryan_j_thomas@hotmail.co.uk>
2021-06-10 11:58:20 +0100

    AMB-722 save external scope to var and then remove + update JS claim validation

--- proxies/live/apiproxy/resources/jsc/create-cache-entry.js
+++ proxies/live/apiproxy/resources/jsc/create-cache-entry.js
@@ -8,7 +8,7 @@ if (context.getVariable('identity-service-config.cis2.simulated_idp') == 'true')
 redirect_uri = context.getVariable('request.queryparam.redirect_uri');
 response_type = context.getVariable('request.queryparam.response_type');
 scope = context.getVariable('oauthv2accesstoken.OAuthV2.GenerateAccessTokenDummy.scope');
-idp = context.getVariable('request.queryparam.scope');
+idp = context.getVariable('idp');
 
 var cacheEntry = {
   client_id: client_id,
