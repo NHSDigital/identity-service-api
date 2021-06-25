d8cadd8ff7ea9e12d0e4da3e199f890bd0db7ce6
jalal <jalalhosseiny@gmail.com>
2021-06-25 14:33:22 +0100

    AMB-751 add splunk fields to token-exchange and client-cerdentials

--- e2e/tests/logging/test_splunk_loggging.py
+++ e2e/tests/logging/test_splunk_loggging.py
@@ -4,7 +4,7 @@ from api_test_utils.apigee_api_trace import ApigeeApiTraceDebug
 from time import time
 from uuid import uuid4
 
-from e2e.scripts.config import OAUTH_URL, HELLO_WORLD_API_URL, ENVIRONMENT
+from e2e.scripts.config import OAUTH_URL, HELLO_WORLD_API_URL, ENVIRONMENT, ID_TOKEN_NHS_LOGIN_PRIVATE_KEY_ABSOLUTE_PATH
 
 
 @pytest.mark.asyncio
@@ -23,9 +23,13 @@ class TestSplunkLogging:
         # Then
         auth_type = await apigee_trace.get_apigee_variable_from_trace(name='accesstoken.auth_type')
         auth_grant_type = await apigee_trace.get_apigee_variable_from_trace(name='accesstoken.auth_grant_type')
+        auth_level = await apigee_trace.get_apigee_variable_from_trace(name='accesstoken.auth_level')
+        auth_provider = await apigee_trace.get_apigee_variable_from_trace(name='accesstoken.auth_provider')
 
         assert auth_type == 'user'
         assert auth_grant_type == 'authorization_code'
+        assert auth_level == 'aal3'
+        assert auth_provider == 'nhs-cis2'
 
     @pytest.mark.happy_path
     @pytest.mark.logging
@@ -53,13 +57,17 @@ class TestSplunkLogging:
         # Then
         auth_type = await apigee_trace.get_apigee_variable_from_trace(name='accesstoken.auth_type')
         auth_grant_type = await apigee_trace.get_apigee_variable_from_trace(name='accesstoken.auth_grant_type')
+        auth_level = await apigee_trace.get_apigee_variable_from_trace(name='accesstoken.auth_level')
+        auth_provider = await apigee_trace.get_apigee_variable_from_trace(name='accesstoken.auth_provider')
 
         assert auth_type == 'app'
         assert auth_grant_type == 'client_credentials'
+        assert auth_level == 'level3'
+        assert auth_provider == 'apim'
 
     @pytest.mark.happy_path
     @pytest.mark.logging
-    async def test_access_token_fields_for_logging_when_using_token_exchange(self):
+    async def test_access_token_fields_for_logging_when_using_token_exchange_cis2(self):
         # Given
         apigee_trace = ApigeeApiTraceDebug(proxy=f"hello-world-{ENVIRONMENT}")
 
@@ -76,6 +84,75 @@ class TestSplunkLogging:
         # Then
         auth_type = await apigee_trace.get_apigee_variable_from_trace(name='accesstoken.auth_type')
         auth_grant_type = await apigee_trace.get_apigee_variable_from_trace(name='accesstoken.auth_grant_type')
+        auth_provider = await apigee_trace.get_apigee_variable_from_trace(name='accesstoken.auth_provider')
+        auth_level = await apigee_trace.get_apigee_variable_from_trace(name='accesstoken.auth_level')
 
         assert auth_type == 'user'
         assert auth_grant_type == 'token_exchange'
+        assert auth_provider == 'nhs-cis2'
+        assert auth_level == 'aal3'
+
+    @pytest.mark.happy_path
+    @pytest.mark.logging
+    @pytest.mark.debug
+    # @pytest.mark.parametrize('scope', ['P9', 'P5', 'P0'])
+    async def test_access_token_fields_for_logging_when_using_token_exchange_nhs_login(self):
+        # Given
+        apigee_trace = ApigeeApiTraceDebug(proxy=f"hello-world-{ENVIRONMENT}")
+
+        id_token_claims = {
+            "aud": "tf_-APIM-1",
+            "id_status": "verified",
+            "token_use": "id",
+            "auth_time": 1616600683,
+            "iss": "https://internal-dev.api.service.nhs.uk",
+            "vot": "P9.Cp.Cd",
+            "exp": int(time()) + 600,
+            "iat": int(time()) - 10,
+            "vtm": "https://auth.sandpit.signin.nhs.uk/trustmark/auth.sandpit.signin.nhs.uk",
+            "jti": "b68ddb28-e440-443d-8725-dfe0da330118",
+            "identity_proofing_level": 'P5'
+        }
+        id_token_headers = {
+            "sub": "49f470a1-cc52-49b7-beba-0f9cec937c46",
+            "aud": "APIM-1",
+            "kid": "nhs-login",
+            "iss": "https://internal-dev.api.service.nhs.uk",
+            "typ": "JWT",
+            "exp": 1616604574,
+            "iat": 1616600974,
+            "alg": "RS512",
+            "jti": "b68ddb28-e440-443d-8725-dfe0da330118",
+        }
+
+        with open(ID_TOKEN_NHS_LOGIN_PRIVATE_KEY_ABSOLUTE_PATH, "r") as f:
+            contents = f.read()
+
+        client_assertion_jwt = self.oauth.create_jwt(kid="test-1")
+        id_token_jwt = self.oauth.create_id_token_jwt(
+            algorithm="RS512",
+            claims=id_token_claims,
+            headers=id_token_headers,
+            signing_key=contents,
+        )
+        resp = await self.oauth.get_token_response(
+            grant_type="token_exchange",
+            _jwt=client_assertion_jwt,
+            id_token_jwt=id_token_jwt,
+        )
+        access_token = resp['body']['access_token']
+
+        # When
+        await apigee_trace.start_trace()
+        requests.get(f"{HELLO_WORLD_API_URL}", headers={"Authorization": f"Bearer {access_token}"})
+
+        # Then
+        auth_type = await apigee_trace.get_apigee_variable_from_trace(name='accesstoken.auth_type')
+        auth_grant_type = await apigee_trace.get_apigee_variable_from_trace(name='accesstoken.auth_grant_type')
+        auth_provider = await apigee_trace.get_apigee_variable_from_trace(name='accesstoken.auth_provider')
+        auth_level = await apigee_trace.get_apigee_variable_from_trace(name='accesstoken.auth_level')
+
+        assert auth_type == 'user'
+        assert auth_grant_type == 'token_exchange'
+        assert auth_provider == 'nhs-login'
+        assert auth_level == 'p9'
--- proxies/live/apiproxy/resources/jsc/set-logging-fields.js
+++ proxies/live/apiproxy/resources/jsc/set-logging-fields.js
@@ -1,18 +1,62 @@
+var grant_type = context.getVariable("request.formparam.grant_type");
 var provider = "unknown";
-var issuer = context.getVariable("jwt.DecodeJWT.FromExternalIdToken.claim.issuer");
-if (issuer) {
-  if (issuer.includes("cis2")) {
-    provider = "cis2";
-  }
-}
-
 var level = "unknown";
-var claim_acr = context.getVariable("jwt.DecodeJWT.FromExternalIdToken.claim.acr");
-if (claim_acr) {
-  if (claim_acr.toLowerCase().includes("aal3")) {
-    level = "aal3";
+
+if (grant_type === "authorization_code") {
+  var issuer = context.getVariable("jwt.DecodeJWT.FromExternalIdToken.claim.issuer");
+  provider = getProvider(issuer);
+
+  var claim_acr = context.getVariable("jwt.DecodeJWT.FromExternalIdToken.claim.acr");
+  level = getLevel(claim_acr);
+
+} else {
+  // Now it's either client-credentials or token-exhange. We can't rely on apigee since, there is no support for token-exchange
+  if (context.getVariable("request.formparam.subject_token")) {
+    // Then it's token-exchange
+    scope = context.getVariable("apigee.user_restricted_scopes");
+    level = getLevel(scope);
+
+    issuer = context.getVariable("idTokenIssuer");
+    provider = getProvider(issuer);
+  } else {
+    scope = context.getVariable("apigee.application_restricted_scopes");
+    level = getLevel(scope);
+
+    provider = "apim";
   }
 }
 
 context.setVariable("splunk.auth.provider", provider);
 context.setVariable("splunk.auth.level", level);
+
+function getLevel(level) {
+  if (level) {
+    level = level.toLowerCase();
+
+    if (level.includes("aal3")) {
+      return "aal3";
+    }
+    if (level.includes(":level3")) {
+      return "level3";
+    }
+    if (level.includes(":p9:")) {
+      return "p9";
+    }
+  }
+  return "unknown";
+}
+
+function getProvider(provider) {
+  if (provider) {
+    provider = provider.toLowerCase();
+    print(provider);
+
+    if (provider.includes("cis2") || provider.includes("nhscis2")) {
+      return "nhs-cis2";
+    }
+    if (provider.includes("nhslogin")) {
+      return "nhs-login";
+    }
+  }
+  return "unknown";
+}
