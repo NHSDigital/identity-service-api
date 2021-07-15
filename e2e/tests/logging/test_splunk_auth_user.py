45b6c8e7d82aa30dd8e79aa9116859e0b762c9b1
jalal <jalalhosseiny@gmail.com>
2021-07-15 15:08:24 +0100

    AMB-776 add tests

--- e2e/tests/logging/test_splunk_auth_user.py
+++ e2e/tests/logging/test_splunk_auth_user.py
@@ -1,12 +1,14 @@
+import base64
+import hashlib
+import hmac
 import pytest
 import requests
 from api_test_utils.apigee_api_trace import ApigeeApiTraceDebug
 from api_test_utils.oauth_helper import OauthHelper
 from urllib.parse import urlparse, urlencode, parse_qsl
 from uuid import uuid4
-import time
 
-from e2e.scripts.config import OAUTH_URL, SERVICE_NAME
+from e2e.scripts.config import OAUTH_URL, SERVICE_NAME, ACCESS_TOKEN_SECRET
 
 
 class Authentication:
@@ -78,14 +80,20 @@ def auth():
     auth.authorization_code_cis2()
 
 
+def calculate_hmac_sha512(content: str, key: str) -> str:
+    binary_content = bytes(content, 'utf-8')
+    hmac_key = bytes(key, 'utf-8')
+    signature = hmac.new(hmac_key, binary_content, hashlib.sha512)
+
+    return base64.b64encode(signature.digest())
+
+
 @pytest.mark.asyncio
 class TestSplunkUserAuthLogging:
     @pytest.mark.happy_path
     @pytest.mark.logging
     @pytest.mark.debug2
-    async def test_populate_access_token_hash(self, test_app_and_product, helper):
-        # async def test_populate_access_token_hash(self):
-        # auth()
+    async def test_populate_hashed_access_token_using_auth_code_cis2(self, test_app_and_product, helper):
         # Given
         p1, p2, test_app = test_app_and_product
         await p1.update_scopes(['urn:nhsd:apim:user-nhs-id:aal3:personal-demographics-service'])
@@ -95,7 +103,6 @@ class TestSplunkUserAuthLogging:
         oauth = OauthHelper(test_app.client_id, test_app.client_secret, callback_url)
 
         apigee_trace = ApigeeApiTraceDebug(proxy=SERVICE_NAME)
-        # apigee_trace.add_trace_filter(header_name="Auto-Test-Header", header_value="flow-callback")
 
         # When
         await apigee_trace.start_trace()
@@ -110,11 +117,9 @@ class TestSplunkUserAuthLogging:
                 "code": await oauth.get_authenticated_with_simulated_auth(),
             },
         )
+        access_token = res.json()["access_token"]
 
         # Then
-        expected_access_token_hash = res.json()["access_token"]
-        access_token_hash = await apigee_trace.get_apigee_variable_from_trace(name='auth.access_token_hash')
-
-        assert access_token_hash != None
-        print(access_token_hash)
-        print(expected_access_token_hash)
+        expected_access_token_hashed = calculate_hmac_sha512(access_token, ACCESS_TOKEN_SECRET).decode('utf-8')
+        actual_access_token_hashed = await apigee_trace.get_apigee_variable_from_trace(name='auth.access_token_hash')
+        assert expected_access_token_hashed == actual_access_token_hashed
