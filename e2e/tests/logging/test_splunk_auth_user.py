456757d550c8dab733984e3259eee54f48906985
Valentinolucas <valentinolucasfantini@gmail.com>
2021-07-23 09:37:13 +0100

    AMB-776 complete test for diferent auth flows

--- e2e/tests/logging/test_splunk_auth_user.py
+++ e2e/tests/logging/test_splunk_auth_user.py
@@ -2,18 +2,21 @@ import base64
 import hashlib
 import hmac
 import pytest
-import requests
+from time import time
 from api_test_utils.apigee_api_trace import ApigeeApiTraceDebug
 from api_test_utils.oauth_helper import OauthHelper
-from urllib.parse import urlparse, urlencode, parse_qsl
-from uuid import uuid4
-
-from e2e.scripts.config import OAUTH_URL, SERVICE_NAME, ACCESS_TOKEN_SECRET, MOCK_IDP_BASE_URL
+from e2e.scripts.config import (
+    OAUTH_URL,
+    SERVICE_NAME,
+    ACCESS_TOKEN_SECRET,
+    MOCK_IDP_BASE_URL,
+    ID_TOKEN_NHS_LOGIN_PRIVATE_KEY_ABSOLUTE_PATH,
+)
 
 
 def calculate_hmac_sha512(content: str, key: str) -> str:
-    binary_content = bytes(content, 'utf-8')
-    hmac_key = bytes(key, 'utf-8')
+    binary_content = bytes(content, "utf-8")
+    hmac_key = bytes(key, "utf-8")
     signature = hmac.new(hmac_key, binary_content, hashlib.sha512)
 
     return base64.b64encode(signature.digest())
@@ -24,10 +27,14 @@ class TestSplunkUserAuthLogging:
     @pytest.mark.happy_path
     @pytest.mark.logging
     @pytest.mark.debug2
-    async def test_populate_hashed_access_token_using_auth_code_cis2(self, test_app_and_product, helper):
+    async def test_populate_hashed_access_token_using_auth_code_cis2(
+        self, test_app_and_product, helper
+    ):
         # Given
         p1, p2, test_app = test_app_and_product
-        await p1.update_scopes(['urn:nhsd:apim:user-nhs-id:aal3:personal-demographics-service'])
+        await p1.update_scopes(
+            ["urn:nhsd:apim:user-nhs-id:aal3:personal-demographics-service"]
+        )
         await p2.update_scopes([])
 
         callback_url = await test_app.get_callback_url()
@@ -51,8 +58,12 @@ class TestSplunkUserAuthLogging:
         access_token = res.json()["access_token"]
 
         # Then
-        expected_access_token_hashed = calculate_hmac_sha512(access_token, ACCESS_TOKEN_SECRET).decode('utf-8')
-        actual_access_token_hashed = await apigee_trace.get_apigee_variable_from_trace(name='auth.access_token_hash')
+        expected_access_token_hashed = calculate_hmac_sha512(
+            access_token, ACCESS_TOKEN_SECRET
+        ).decode("utf-8")
+        actual_access_token_hashed = await apigee_trace.get_apigee_variable_from_trace(
+            name="auth.access_token_hash"
+        )
         print(expected_access_token_hashed)
         print(actual_access_token_hashed)
         assert expected_access_token_hashed == actual_access_token_hashed
@@ -60,7 +71,9 @@ class TestSplunkUserAuthLogging:
     @pytest.mark.happy_path
     @pytest.mark.logging
     @pytest.mark.debug2
-    async def test_populate_hashed_access_token_using_auth_code_nhs_login(self, test_app_and_product, helper):
+    async def test_populate_hashed_access_token_using_auth_code_nhs_login(
+        self, test_app_and_product, helper
+    ):
         # Given
         # Make authorize request to retrieve state2
         response = await self.oauth.hit_oauth_endpoint(
@@ -71,7 +84,7 @@ class TestSplunkUserAuthLogging:
                 "redirect_uri": self.oauth.redirect_uri,
                 "response_type": "code",
                 "state": "1234567890",
-                "scope": "nhs-login"
+                "scope": "nhs-login",
             },
             allow_redirects=False,
         )
@@ -92,10 +105,7 @@ class TestSplunkUserAuthLogging:
                 "state": state,
             },
             headers={"Content-Type": "application/x-www-form-urlencoded"},
-            data={
-                "state": state,
-                "auth_method": "P9"
-            },
+            data={"state": state, "auth_method": "P9"},
             allow_redirects=False,
         )
 
@@ -128,14 +138,122 @@ class TestSplunkUserAuthLogging:
                 "code": auth_code,
                 "redirect_uri": self.oauth.redirect_uri,
                 "client_id": self.oauth.client_id,
-                "client_secret": self.oauth.client_secret
+                "client_secret": self.oauth.client_secret,
             },
             allow_redirects=False,
         )
 
-        access_token = response['body']['access_token']
+        access_token = response["body"]["access_token"]
 
         # Then
-        expected_access_token_hashed = calculate_hmac_sha512(access_token, ACCESS_TOKEN_SECRET).decode('utf-8')
-        actual_access_token_hashed = await apigee_trace.get_apigee_variable_from_trace(name='auth.access_token_hash')
+        expected_access_token_hashed = calculate_hmac_sha512(
+            access_token, ACCESS_TOKEN_SECRET
+        ).decode("utf-8")
+        actual_access_token_hashed = await apigee_trace.get_apigee_variable_from_trace(
+            name="auth.access_token_hash"
+        )
+        assert expected_access_token_hashed == actual_access_token_hashed
+
+    @pytest.mark.happy_path
+    @pytest.mark.logging
+    @pytest.mark.debug2
+    async def test_populate_hashed_access_token_using_client_credentials(self):
+        jwt = self.oauth.create_jwt(kid="test-1")
+        apigee_trace = ApigeeApiTraceDebug(proxy=SERVICE_NAME)
+        await apigee_trace.start_trace()
+        response = await self.oauth.get_token_response("client_credentials", _jwt=jwt)
+        access_token = response["body"]["access_token"]
+        expected_access_token_hashed = calculate_hmac_sha512(
+            access_token, ACCESS_TOKEN_SECRET
+        ).decode("utf-8")
+        actual_access_token_hashed = await apigee_trace.get_apigee_variable_from_trace(
+            name="auth.access_token_hash"
+        )
+        assert expected_access_token_hashed == actual_access_token_hashed
+
+    @pytest.mark.happy_path
+    @pytest.mark.logging
+    @pytest.mark.debug2
+    async def test_populate_hashed_access_token_using_token_exchange_cis2(self):
+
+        id_token_jwt = self.oauth.create_id_token_jwt()
+        client_assertion_jwt = self.oauth.create_jwt(kid="test-1")
+        apigee_trace = ApigeeApiTraceDebug(proxy=SERVICE_NAME)
+        await apigee_trace.start_trace()
+        response = await self.oauth.get_token_response(
+            grant_type="token_exchange",
+            _jwt=client_assertion_jwt,
+            id_token_jwt=id_token_jwt,
+        )
+        access_token = response["body"]["access_token"]
+        expected_access_token_hashed = calculate_hmac_sha512(
+            access_token, ACCESS_TOKEN_SECRET
+        ).decode("utf-8")
+        actual_access_token_hashed = await apigee_trace.get_apigee_variable_from_trace(
+            name="auth.access_token_hash"
+        )
+        assert expected_access_token_hashed == actual_access_token_hashed
+
+    @pytest.mark.happy_path
+    @pytest.mark.logging
+    @pytest.mark.debug2
+    async def test_populate_hashed_access_token_using_token_exchange_nhs_login(self):
+
+        id_token_claims = {
+            "aud": "tf_-APIM-1",
+            "id_status": "verified",
+            "token_use": "id",
+            "auth_time": 1616600683,
+            "iss": "https://internal-dev.api.service.nhs.uk",
+            "vot": "P9.Cp.Cd",
+            "exp": int(time()) + 600,
+            "iat": int(time()) - 10,
+            "vtm": "https://auth.sandpit.signin.nhs.uk/trustmark/auth.sandpit.signin.nhs.uk",
+            "jti": "b68ddb28-e440-443d-8725-dfe0da330118",
+            "identity_proofing_level": "P9",
+        }
+        id_token_headers = {
+            "sub": "49f470a1-cc52-49b7-beba-0f9cec937c46",
+            "aud": "APIM-1",
+            "kid": "nhs-login",
+            "iss": "https://internal-dev.api.service.nhs.uk",
+            "typ": "JWT",
+            "exp": 1616604574,
+            "iat": 1616600974,
+            "alg": "RS512",
+            "jti": "b68ddb28-e440-443d-8725-dfe0da330118",
+        }
+
+        with open(ID_TOKEN_NHS_LOGIN_PRIVATE_KEY_ABSOLUTE_PATH, "r") as f:
+            contents = f.read()
+
+        client_assertion_jwt = self.oauth.create_jwt(kid="test-1")
+        id_token_jwt = self.oauth.create_id_token_jwt(
+            algorithm="RS512",
+            claims=id_token_claims,
+            headers=id_token_headers,
+            signing_key=contents,
+        )
+
+        # When
+        apigee_trace = ApigeeApiTraceDebug(proxy=SERVICE_NAME)
+        await apigee_trace.start_trace()
+        response = await self.oauth.get_token_response(
+            grant_type="token_exchange",
+            data={
+                "grant_type": "urn:ietf:params:oauth:grant-type:token-exchange",
+                "subject_token_type": "urn:ietf:params:oauth:token-type:id_token",
+                "client_assertion_type": "urn:ietf:params:oauth:client-assertion-type:jwt-bearer",
+                "subject_token": id_token_jwt,
+                "client_assertion": client_assertion_jwt,
+            },
+        )
+
+        access_token = response["body"]["access_token"]
+        expected_access_token_hashed = calculate_hmac_sha512(
+            access_token, ACCESS_TOKEN_SECRET
+        ).decode("utf-8")
+        actual_access_token_hashed = await apigee_trace.get_apigee_variable_from_trace(
+            name="auth.access_token_hash"
+        )
         assert expected_access_token_hashed == actual_access_token_hashed
--- e2e/tests/logging/test_splunk_loggging.py
+++ e2e/tests/logging/test_splunk_loggging.py
@@ -4,38 +4,59 @@ from api_test_utils.apigee_api_trace import ApigeeApiTraceDebug
 from time import time
 from uuid import uuid4
 
-from e2e.scripts.config import OAUTH_URL, HELLO_WORLD_API_URL, ENVIRONMENT, ID_TOKEN_NHS_LOGIN_PRIVATE_KEY_ABSOLUTE_PATH, MOCK_IDP_BASE_URL
+from e2e.scripts.config import (
+    OAUTH_URL,
+    HELLO_WORLD_API_URL,
+    ENVIRONMENT,
+    ID_TOKEN_NHS_LOGIN_PRIVATE_KEY_ABSOLUTE_PATH,
+    MOCK_IDP_BASE_URL,
+)
 
 
 @pytest.mark.asyncio
 class TestSplunkLogging:
     @pytest.mark.happy_path
     @pytest.mark.logging
-    @pytest.mark.usefixtures('set_access_token')
+    @pytest.mark.usefixtures("set_access_token")
     @pytest.mark.debug1
-    async def test_access_token_fields_for_logging_when_using_authorization_code_cis2(self, helper):
+    async def test_access_token_fields_for_logging_when_using_authorization_code_cis2(
+        self, helper
+    ):
         # Given
         apigee_trace = ApigeeApiTraceDebug(proxy=f"hello-world-{ENVIRONMENT}")
 
         # When
         await apigee_trace.start_trace()
-        requests.get(f"{HELLO_WORLD_API_URL}", headers={"Authorization": f"Bearer {self.oauth.access_token}"})
+        requests.get(
+            f"{HELLO_WORLD_API_URL}",
+            headers={"Authorization": f"Bearer {self.oauth.access_token}"},
+        )
 
         # Then
-        auth_type = await apigee_trace.get_apigee_variable_from_trace(name='accesstoken.auth_type')
-        auth_grant_type = await apigee_trace.get_apigee_variable_from_trace(name='accesstoken.auth_grant_type')
-        auth_level = await apigee_trace.get_apigee_variable_from_trace(name='accesstoken.auth_level')
-        auth_provider = await apigee_trace.get_apigee_variable_from_trace(name='accesstoken.auth_provider')
+        auth_type = await apigee_trace.get_apigee_variable_from_trace(
+            name="accesstoken.auth_type"
+        )
+        auth_grant_type = await apigee_trace.get_apigee_variable_from_trace(
+            name="accesstoken.auth_grant_type"
+        )
+        auth_level = await apigee_trace.get_apigee_variable_from_trace(
+            name="accesstoken.auth_level"
+        )
+        auth_provider = await apigee_trace.get_apigee_variable_from_trace(
+            name="accesstoken.auth_provider"
+        )
 
-        assert auth_type == 'user'
-        assert auth_grant_type == 'authorization_code'
-        assert auth_level == 'aal3'
-        assert auth_provider == 'nhs-cis2'
+        assert auth_type == "user"
+        assert auth_grant_type == "authorization_code"
+        assert auth_level == "aal3"
+        assert auth_provider == "nhs-cis2"
 
     @pytest.mark.happy_path
     @pytest.mark.logging
-    @pytest.mark.parametrize('scope', ['P9', 'P5', 'P0'])
-    async def test_access_token_fields_for_logging_when_using_authorization_code_nhs_login(self, scope, helper):
+    @pytest.mark.parametrize("scope", ["P9", "P5", "P0"])
+    async def test_access_token_fields_for_logging_when_using_authorization_code_nhs_login(
+        self, scope, helper
+    ):
         # Given
         apigee_trace = ApigeeApiTraceDebug(proxy=f"hello-world-{ENVIRONMENT}")
 
@@ -48,7 +69,7 @@ class TestSplunkLogging:
                 "redirect_uri": self.oauth.redirect_uri,
                 "response_type": "code",
                 "state": "1234567890",
-                "scope": "nhs-login"
+                "scope": "nhs-login",
             },
             allow_redirects=False,
         )
@@ -69,10 +90,7 @@ class TestSplunkLogging:
                 "state": state,
             },
             headers={"Content-Type": "application/x-www-form-urlencoded"},
-            data={
-                "state": state,
-                "auth_method": scope
-            },
+            data={"state": state, "auth_method": scope},
             allow_redirects=False,
         )
 
@@ -101,26 +119,37 @@ class TestSplunkLogging:
                 "code": auth_code,
                 "redirect_uri": self.oauth.redirect_uri,
                 "client_id": self.oauth.client_id,
-                "client_secret": self.oauth.client_secret
+                "client_secret": self.oauth.client_secret,
             },
             allow_redirects=False,
         )
 
-        access_token = response['body']['access_token']
+        access_token = response["body"]["access_token"]
         # When
         await apigee_trace.start_trace()
-        requests.get(f"{HELLO_WORLD_API_URL}", headers={"Authorization": f"Bearer {access_token}"})
+        requests.get(
+            f"{HELLO_WORLD_API_URL}",
+            headers={"Authorization": f"Bearer {access_token}"},
+        )
 
         # Then
-        auth_type = await apigee_trace.get_apigee_variable_from_trace(name='accesstoken.auth_type')
-        auth_grant_type = await apigee_trace.get_apigee_variable_from_trace(name='accesstoken.auth_grant_type')
-        auth_level = await apigee_trace.get_apigee_variable_from_trace(name='accesstoken.auth_level')
-        auth_provider = await apigee_trace.get_apigee_variable_from_trace(name='accesstoken.auth_provider')
+        auth_type = await apigee_trace.get_apigee_variable_from_trace(
+            name="accesstoken.auth_type"
+        )
+        auth_grant_type = await apigee_trace.get_apigee_variable_from_trace(
+            name="accesstoken.auth_grant_type"
+        )
+        auth_level = await apigee_trace.get_apigee_variable_from_trace(
+            name="accesstoken.auth_level"
+        )
+        auth_provider = await apigee_trace.get_apigee_variable_from_trace(
+            name="accesstoken.auth_provider"
+        )
 
-        assert auth_type == 'user'
-        assert auth_grant_type == 'authorization_code'
+        assert auth_type == "user"
+        assert auth_grant_type == "authorization_code"
         assert auth_level == scope.lower()
-        assert auth_provider == 'apim-mock'
+        assert auth_provider == "apim-mock"
 
     @pytest.mark.happy_path
     @pytest.mark.logging
@@ -128,33 +157,46 @@ class TestSplunkLogging:
         # Given
         apigee_trace = ApigeeApiTraceDebug(proxy=f"hello-world-{ENVIRONMENT}")
         jwt_claims = {
-            'kid': 'test-1',
-            'claims': {
+            "kid": "test-1",
+            "claims": {
                 "sub": self.oauth.client_id,
                 "iss": self.oauth.client_id,
                 "jti": str(uuid4()),
                 "aud": f"{OAUTH_URL}/token",
                 "exp": int(time()),
-            }
+            },
         }
         jwt = self.oauth.create_jwt(**jwt_claims)
-        resp = await self.oauth.get_token_response(grant_type='client_credentials', _jwt=jwt)
-        access_token = resp['body']['access_token']
+        resp = await self.oauth.get_token_response(
+            grant_type="client_credentials", _jwt=jwt
+        )
+        access_token = resp["body"]["access_token"]
 
         # When
         await apigee_trace.start_trace()
-        requests.get(f"{HELLO_WORLD_API_URL}", headers={"Authorization": f"Bearer {access_token}"})
+        requests.get(
+            f"{HELLO_WORLD_API_URL}",
+            headers={"Authorization": f"Bearer {access_token}"},
+        )
 
         # Then
-        auth_type = await apigee_trace.get_apigee_variable_from_trace(name='accesstoken.auth_type')
-        auth_grant_type = await apigee_trace.get_apigee_variable_from_trace(name='accesstoken.auth_grant_type')
-        auth_level = await apigee_trace.get_apigee_variable_from_trace(name='accesstoken.auth_level')
-        auth_provider = await apigee_trace.get_apigee_variable_from_trace(name='accesstoken.auth_provider')
+        auth_type = await apigee_trace.get_apigee_variable_from_trace(
+            name="accesstoken.auth_type"
+        )
+        auth_grant_type = await apigee_trace.get_apigee_variable_from_trace(
+            name="accesstoken.auth_grant_type"
+        )
+        auth_level = await apigee_trace.get_apigee_variable_from_trace(
+            name="accesstoken.auth_level"
+        )
+        auth_provider = await apigee_trace.get_apigee_variable_from_trace(
+            name="accesstoken.auth_provider"
+        )
 
-        assert auth_type == 'app'
-        assert auth_grant_type == 'client_credentials'
-        assert auth_level == 'level3'
-        assert auth_provider == 'apim'
+        assert auth_type == "app"
+        assert auth_grant_type == "client_credentials"
+        assert auth_level == "level3"
+        assert auth_provider == "apim"
 
     @pytest.mark.happy_path
     @pytest.mark.logging
@@ -163,30 +205,46 @@ class TestSplunkLogging:
         apigee_trace = ApigeeApiTraceDebug(proxy=f"hello-world-{ENVIRONMENT}")
 
         id_token_jwt = self.oauth.create_id_token_jwt()
-        client_assertion_jwt = self.oauth.create_jwt(kid='test-1')
-        resp = await self.oauth.get_token_response(grant_type='token_exchange', _jwt=client_assertion_jwt,
-                                                   id_token_jwt=id_token_jwt)
-        access_token = resp['body']['access_token']
+        client_assertion_jwt = self.oauth.create_jwt(kid="test-1")
+        resp = await self.oauth.get_token_response(
+            grant_type="token_exchange",
+            _jwt=client_assertion_jwt,
+            id_token_jwt=id_token_jwt,
+        )
+        access_token = resp["body"]["access_token"]
 
         # When
         await apigee_trace.start_trace()
-        requests.get(f"{HELLO_WORLD_API_URL}", headers={"Authorization": f"Bearer {access_token}"})
+        requests.get(
+            f"{HELLO_WORLD_API_URL}",
+            headers={"Authorization": f"Bearer {access_token}"},
+        )
 
         # Then
-        auth_type = await apigee_trace.get_apigee_variable_from_trace(name='accesstoken.auth_type')
-        auth_grant_type = await apigee_trace.get_apigee_variable_from_trace(name='accesstoken.auth_grant_type')
-        auth_provider = await apigee_trace.get_apigee_variable_from_trace(name='accesstoken.auth_provider')
-        auth_level = await apigee_trace.get_apigee_variable_from_trace(name='accesstoken.auth_level')
+        auth_type = await apigee_trace.get_apigee_variable_from_trace(
+            name="accesstoken.auth_type"
+        )
+        auth_grant_type = await apigee_trace.get_apigee_variable_from_trace(
+            name="accesstoken.auth_grant_type"
+        )
+        auth_provider = await apigee_trace.get_apigee_variable_from_trace(
+            name="accesstoken.auth_provider"
+        )
+        auth_level = await apigee_trace.get_apigee_variable_from_trace(
+            name="accesstoken.auth_level"
+        )
 
-        assert auth_type == 'user'
-        assert auth_grant_type == 'token_exchange'
-        assert auth_provider == 'nhs-cis2'
-        assert auth_level == 'aal3'
+        assert auth_type == "user"
+        assert auth_grant_type == "token_exchange"
+        assert auth_provider == "nhs-cis2"
+        assert auth_level == "aal3"
 
     @pytest.mark.happy_path
     @pytest.mark.logging
-    @pytest.mark.parametrize('scope', ['P9', 'P5', 'P0'])
-    async def test_access_token_fields_for_logging_when_using_token_exchange_nhs_login(self, scope):
+    @pytest.mark.parametrize("scope", ["P9", "P5", "P0"])
+    async def test_access_token_fields_for_logging_when_using_token_exchange_nhs_login(
+        self, scope
+    ):
         # Given
         apigee_trace = ApigeeApiTraceDebug(proxy=f"hello-world-{ENVIRONMENT}")
 
@@ -201,7 +259,7 @@ class TestSplunkLogging:
             "iat": int(time()) - 10,
             "vtm": "https://auth.sandpit.signin.nhs.uk/trustmark/auth.sandpit.signin.nhs.uk",
             "jti": str(uuid4()),
-            "identity_proofing_level": scope
+            "identity_proofing_level": scope,
         }
         id_token_headers = {
             "sub": "49f470a1-cc52-49b7-beba-0f9cec937c46",
@@ -230,19 +288,30 @@ class TestSplunkLogging:
             _jwt=client_assertion_jwt,
             id_token_jwt=id_token_jwt,
         )
-        access_token = resp['body']['access_token']
+        access_token = resp["body"]["access_token"]
 
         # When
         await apigee_trace.start_trace()
-        requests.get(f"{HELLO_WORLD_API_URL}", headers={"Authorization": f"Bearer {access_token}"})
+        requests.get(
+            f"{HELLO_WORLD_API_URL}",
+            headers={"Authorization": f"Bearer {access_token}"},
+        )
 
         # Then
-        auth_type = await apigee_trace.get_apigee_variable_from_trace(name='accesstoken.auth_type')
-        auth_grant_type = await apigee_trace.get_apigee_variable_from_trace(name='accesstoken.auth_grant_type')
-        auth_provider = await apigee_trace.get_apigee_variable_from_trace(name='accesstoken.auth_provider')
-        auth_level = await apigee_trace.get_apigee_variable_from_trace(name='accesstoken.auth_level')
+        auth_type = await apigee_trace.get_apigee_variable_from_trace(
+            name="accesstoken.auth_type"
+        )
+        auth_grant_type = await apigee_trace.get_apigee_variable_from_trace(
+            name="accesstoken.auth_grant_type"
+        )
+        auth_provider = await apigee_trace.get_apigee_variable_from_trace(
+            name="accesstoken.auth_provider"
+        )
+        auth_level = await apigee_trace.get_apigee_variable_from_trace(
+            name="accesstoken.auth_level"
+        )
 
-        assert auth_type == 'user'
-        assert auth_grant_type == 'token_exchange'
-        assert auth_provider == 'nhs-login'
+        assert auth_type == "user"
+        assert auth_grant_type == "token_exchange"
+        assert auth_provider == "nhs-login"
         assert auth_level == scope.lower()
