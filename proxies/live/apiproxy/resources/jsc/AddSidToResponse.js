2ec38ac9fcbc530e0219a67dc77c9290e6ed29cd
Josh Finch <me@joshfinch.com>
2021-11-08 14:41:41 +0000

    Moved SID check to /token

--- proxies/live/apiproxy/resources/jsc/AddSidToResponse.js
+++ proxies/live/apiproxy/resources/jsc/AddSidToResponse.js
@@ -1,8 +1,9 @@
 //Parse the respose from the target.
 var res = JSON.parse(context.proxyResponse.content);
+var sid = context.getVariable('jwt.DecodeJWT.FromExternalIdToken.decoded.claim.sid');
 
 //Add dummy sid value to response
-res.sid = 'not a real sid'
+res.sid = sid
           
 //Set the response variable. 
 context.proxyResponse.content = JSON.stringify(res);
