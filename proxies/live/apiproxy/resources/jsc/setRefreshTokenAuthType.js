627fec57881160f64ce9604371d45c7fb38ee59d
Alex Carrie <acarrie.dev@gmail.com>
2021-12-24 15:57:33 +0000

    APM-657 Remove casting to string

--- proxies/live/apiproxy/resources/jsc/setRefreshTokenAuthType.js
+++ proxies/live/apiproxy/resources/jsc/setRefreshTokenAuthType.js
@@ -1,4 +1,4 @@
-const authProvider = String(context.getVariable('session.auth_provider'));
+const authProvider = context.getVariable('session.auth_provider');
 
 if (authProvider !== null && authProvider === "nhs-cis2") {
     var refreshTokenDefaultMs = parseInt(context.getVariable('identity-service-config.cis2.refresh_token_expiry_ms'));
