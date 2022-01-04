ea61d3f70d619268f17c75a33d99d498696ea16b
Alex Carrie <acarrie.dev@gmail.com>
2022-01-04 11:23:38 +0000

    APM-657 Dont parse kvms

--- proxies/live/apiproxy/resources/jsc/setRefreshTokenAuthType.js
+++ proxies/live/apiproxy/resources/jsc/setRefreshTokenAuthType.js
@@ -1,11 +1,11 @@
 const authProvider = context.getVariable('session.auth_provider');
 
 if (authProvider !== null && authProvider === "nhs-cis2") {
-    var refreshTokenDefaultMs = parseInt(context.getVariable('identity-service-config.cis2.refresh_token_expiry_ms'));
-    var refreshTokenDefaultValidityMs = parseInt(context.getVariable('identity-service-config.cis2.refresh_tokens_validity_ms'));
+    var refreshTokenDefaultMs = context.getVariable('identity-service-config.cis2.refresh_token_expiry_ms');
+    var refreshTokenDefaultValidityMs = context.getVariable('identity-service-config.cis2.refresh_tokens_validity_ms');
 } else {
-    var refreshTokenDefaultMs = parseInt(context.getVariable('identity-service-config.nhs_login.refresh_token_expiry_ms'));
-    var refreshTokenDefaultValidityMs = parseInt(context.getVariable('identity-service-config.nhs_login.refresh_tokens_validity_ms'));
+    var refreshTokenDefaultMs = context.getVariable('identity-service-config.nhs_login.refresh_token_expiry_ms');
+    var refreshTokenDefaultValidityMs = context.getVariable('identity-service-config.nhs_login.refresh_tokens_validity_ms');
 }
 
 context.setVariable('apigee.refresh_token_expiry_ms', refreshTokenDefaultMs);
