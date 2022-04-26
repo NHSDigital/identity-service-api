fcff6d87226d2ed8c72387564eda392444f7d5ab
Valentinolucas <valentinolucasfantini@gmail.com>
2022-04-26 13:45:16 +0100

    APM-1676 add status endpoint tests

--- e2e/tests/oauth/test_oauth_endpoints.py
+++ e2e/tests/oauth/test_oauth_endpoints.py
@@ -665,14 +665,40 @@ class TestOauthEndpoints:
             endpoint="_ping",
         )
 
-    async def test_status(self, helper):
+    @pytest.mark.parametrize("test_case", [
+        {
+            # Condition 1 Happy path
+            "expected_status_code": 200,
+            "expected_response": ["status", "version", "revision", "releaseId", "commitId", "checks"],
+            "headers":{"apikey": f"{STATUS_ENDPOINT_API_KEY}"}
+        },
+        {
+            # Condition 2 invalid api key
+            "expected_status_code": 401,
+            "expected_response": {
+                "error": "Access Denied",
+                "error_description": "Invalid api key for _status monitoring endpoint"
+            },
+            "headers": {"apikey": "invalid"}
+        },
+        {
+            # Condition 3 invalid api key header
+            "expected_status_code": 401,
+            "expected_response": {
+                "error": "Access Denied",
+                "error_description": "Invalid api key for _status monitoring endpoint"
+            },
+            "headers": {"invalid": "invalid"}
+        }
+    ])
+    async def test_status(self, helper, test_case):
         assert await helper.send_request_and_check_output(
-            expected_status_code=200,
+            expected_status_code=test_case["expected_status_code"],
             function=self.oauth.hit_oauth_endpoint,
-            expected_response=["status", "version", "revision", "releaseId", "commitId", "checks"],
+            expected_response=test_case["expected_response"],
             method="GET",
             endpoint="_status",
-            headers={"apikey": f"{STATUS_ENDPOINT_API_KEY}"}
+            headers=test_case["headers"]
         )
 
 
