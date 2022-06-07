6d3cfea9ad02124b0680d23dd8e5cd37ca109918
hirra farooq <hirra.farooq1@nhs.net>
2022-06-07 17:51:46 +0100

    AMB-1136: use kvm value to conditionally set nhs login access token expiry time

--- proxies/live/apiproxy/resources/jsc/setDefaultAccessAndRefreshTokenTimes.js
+++ proxies/live/apiproxy/resources/jsc/setDefaultAccessAndRefreshTokenTimes.js
@@ -3,10 +3,13 @@ const authProvider = context.getVariable('apigee.auth_provider');
 if (authProvider !== null && authProvider === "nhs-cis2" ) {
     var refreshTokenDefaultMs = context.getVariable('identity-service-config.cis2.refresh_token_expiry_ms');
     var refreshTokenDefaultValidityMs = context.getVariable('identity-service-config.cis2.refresh_tokens_validity_ms');
+    var accessTokenDefaultMs = context.getVariable('identity-service-config.cis2.access_token_expiry_ms');
 } else {
     var refreshTokenDefaultMs = context.getVariable('identity-service-config.nhs_login.refresh_token_expiry_ms');
     var refreshTokenDefaultValidityMs = context.getVariable('identity-service-config.nhs_login.refresh_tokens_validity_ms');
+    var accessTokenDefaultMs = context.getVariable('identity-service-config.nhs_login.access_token_expiry_ms');
 }
 
+context.setVariable('apigee.access_token_expiry_ms', accessTokenDefaultMs)
 context.setVariable('apigee.refresh_token_expiry_ms', refreshTokenDefaultMs);
 context.setVariable('apigee.refresh_tokens_validity_ms', refreshTokenDefaultValidityMs);
