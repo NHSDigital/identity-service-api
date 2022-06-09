aa6d538f8121fbae0203f7258852c4cc2f8debe8
hirra farooq <hirra.farooq1@nhs.net>
2022-06-09 10:29:01 +0100

    AMB-1136: using assign message plicy to set flow variables rather than js

--- proxies/live/apiproxy/resources/jsc/selectIdTokenFieldsTokenExchange.js
+++
@@ -1,7 +0,0 @@
-var exp = context.getVariable('jwt.DecodeJWT.FromSubjectTokenFormParam.decoded.claim.exp');
-var iss = context.getVariable('jwt.DecodeJWT.FromSubjectTokenFormParam.decoded.claim.iss');
-var aud = context.getVariable('jwt.DecodeJWT.FromSubjectTokenFormParam.decoded.claim.aud');
-
-context.setVariable('idtoken_decoded_claim_exp', exp)
-context.setVariable('idtoken_decoded_claim_iss', iss);
-context.setVariable('idtoken_decoded_claim_aud', aud);
--- proxies/live/apiproxy/resources/jsc/setRefreshTokenAuthProvider.js
+++
@@ -1,2 +0,0 @@
-const authProvider = context.getVariable("oauthv2refreshtoken.GetOAuthV2Info.RefreshTokenAttributes.accesstoken.auth_provider");
-context.setVariable("apigee.auth_provider", authProvider);
