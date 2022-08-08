aa43cf4c751305e6b1da1c24b4ce6b599383ae2f
Jack McCaffrey <jack.mccaffrey1@nhs.net>
2022-08-08 12:25:39 +0100

    AMB-1252: TEMP DO NOT MERGE - hardcoding jwks url for testing

--- proxies/live/apiproxy/resources/jsc/setServiceCalloutUrl.js
+++ proxies/live/apiproxy/resources/jsc/setServiceCalloutUrl.js
@@ -1,4 +1,4 @@
-var urlvalue = context.getVariable('verifyapikey.VerifyAPIKey.FromJWT.jwks-resource-url');
+var urlvalue = 'https://raw.githubusercontent.com/NHSDigital/identity-service-jwks/6c88da16dd898ab53b5f5ea97174c826a35d10b7/jwks/int/f6bcd6f9-1eb5-40c2-9020-4d57ea73f911.json';//'verifyapikey.VerifyAPIKey.FromJWT.jwks-resource-url');
 var re = new RegExp('^(https?:\/\/[^/]+)(\/.*)$');
 var match = re.exec(urlvalue);
 if (match) {
