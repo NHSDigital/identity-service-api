e41fbd44bc6215baf449a262e12fc96e1cca6fe5
Valentinolucas <valentinolucasfantini@gmail.com>
2022-09-01 10:39:52 +0100

    APM-2975 fix test exp in the past

--- e2e/tests/oauth/test_client_credentials.py
+++ e2e/tests/oauth/test_client_credentials.py
@@ -194,7 +194,7 @@ class TestClientCredentialsJWT:
                     "error_description": "Invalid exp claim in JWT - JWT has expired",
                 },
                 400,
-                {"exp": int(time()) - 600},
+                {"exp": 0},
             ),
             (
                 # Test exp above 5 min
