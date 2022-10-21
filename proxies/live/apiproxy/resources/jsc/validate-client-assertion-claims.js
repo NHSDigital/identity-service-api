d5c5959c9bf8e0cb252454ca5a5d0591ce38ac5b
Ed Feldman Bright <ed.feldmanbright@nhs.net>
2022-10-21 11:29:50 +0000

    (Refactor) Move MissingKid check to validation script

--- proxies/live/apiproxy/resources/jsc/validate-client-assertion-claims.js
+++ proxies/live/apiproxy/resources/jsc/validate-client-assertion-claims.js
@@ -1,10 +1,22 @@
-decodedJwt = context.getVariable('jwt.DecodeJWT.FromClientAssertionFormParam.payload-json');
-decodedJwt = JSON.parse(decodedJwt);
 
-var InvalidExpiryTime = true
-
-if(typeof decodedJwt.exp == "number"){
-    InvalidExpiryTime = false
+function extractJsonVariable(contextVariableName) {
+  return JSON.parse(
+    context.getVariable(
+      "jwt.DecodeJWT.FromClientAssertionFormParam."
+      + contextVariableName
+    )
+  )
 }
 
+jwtHeaders = extractJsonVariable('header-json')
+jwtPayload = extractJsonVariable('payload-json')
+
+var MissingKidHeader = true
+var InvalidExpiryTime = true
+
+if(typeof jwtPayload.exp == "number") InvalidExpiryTime = false
+
+if (jwtHeaders.kid) MissingKidHeader = false
+
+context.setVariable('InvalidJwt.MissingKidHeader', MissingKidHeader);
 context.setVariable('InvalidJwt.InvalidExpiryTimeType', InvalidExpiryTime);
