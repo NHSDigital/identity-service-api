51867d96ca6b676b685cf74d9186befe28163016
Ed Feldman Bright <ed.feldmanbright@nhs.net>
2022-10-22 15:59:28 +0000

    Refactor other validation checks to JS and apply new flow to ClientCredentials

--- proxies/live/apiproxy/resources/jsc/validate-client-assertion-claims.js
+++ proxies/live/apiproxy/resources/jsc/validate-client-assertion-claims.js
@@ -12,5 +12,13 @@ jwtHeaders = extractJsonVariable('header-json')
 jwtPayload = extractJsonVariable('payload-json')
 
 // Set context variables based on the condition in the second argument
-context.setVariable('InvalidJwt.MissingKidHeader', jwtPayload.exp == "number");
-context.setVariable('InvalidJwt.InvalidExpiryTime', !!jwtHeaders.kid);
+context.setVariable('InvalidJwt.MissingKidHeader', !jwtHeaders.kid);
+context.setVariable('InvalidJwt.MissingOrInvalidTypHeader', jwtHeaders.typ != "jwt");
+context.setVariable('InvalidJwt.MissingExpClaim', !jwtPayload.exp);
+context.setVariable('InvalidJwt.InvalidExpiryTime', typeof jwtPayload.exp != "number");
+
+// We advise to limit expiry time to now + 5 minutes. Allowing an extra 10 seconds to mitigate edge cases:
+context.setVariable('InvalidJwt.ExpClaimTooLong', typeof jwtPayload.exp == "number" && jwtPayload.exp > 310);
+
+context.setVariable('InvalidJwt.MissingOrInvalidIssClaim', !jwtPayload.iss || jwtPayload.iss != jwtPayload.sub);
+context.setVariable('InvalidJwt.MissingJtiClaim', !jwtPayload.jti);
