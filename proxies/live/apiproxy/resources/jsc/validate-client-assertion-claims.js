6eb22d145259b17533228e1dfe086e16b5f59232
Ed Feldman Bright <ed.feldmanbright@nhs.net>
2022-10-26 11:09:56 +0000

    Refactor current JWT errors

--- proxies/live/apiproxy/resources/jsc/validate-client-assertion-claims.js
+++ proxies/live/apiproxy/resources/jsc/validate-client-assertion-claims.js
@@ -1,9 +1,3 @@
-// Declare error message strings
-missingKidCondition = !jwtHeaders.kid
-missingKidMessage = "Missing 'kid' header in JWT"
-noErrorMessage = ""
-
-
 function extractJsonVariable(contextVariableName) {
   return JSON.parse(
     context.getVariable(
@@ -16,23 +10,39 @@ function extractJsonVariable(contextVariableName) {
 
 jwtHeaders = extractJsonVariable('header-json')
 jwtPayload = extractJsonVariable('payload-json')
+expExpiry = context.getVariable("jwt.DecodeJWT.FromClientAssertionFormParam.seconds_remaining")
 
-// Set context variables based on the condition in the second argument
+// Declare error message strings
+missingKidMessage = "Missing 'kid' header in JWT"
+missingOrInvalidTypMessage = "Invalid 'typ' header in JWT - must be 'JWT'"
+missingExpClaimMessage = "Missing exp claim in JWT"
+invalidExpiryTimeMessage = "Exp claim must be an integer"
+missingOrInvalidIssClaimMessage = "Missing or non-matching iss/sub claims in JWT"
+missingJtiClaimMessage = "Missing jti claim in JWT"
+invalidJtiMessage = "Jti claim must be a unique string value such as a GUID"
+expClaimTooLongMessage = "Invalid exp claim in JWT - more than 5 minutes in future"
+noErrorMessage = ""
 
-context.setVariable(
-  'InvalidJwt.ErrorMessage',
+// Set conditions for trigger error messages
+missingKidCondition = !jwtHeaders.kid
+missingOrInvalidTypCondition = typeof jwtHeaders.typ != "string" || jwtHeaders.typ.toLowerCase() != "jwt"
+missingExpClaimCondition = !jwtPayload.exp
+invalidExpiryTimeCondition = typeof jwtPayload.exp != "number"
+missingOrInvalidIssClaimCondition = !jwtPayload.iss || jwtPayload.iss != jwtPayload.sub
+missingJtiClaimCondition = !jwtPayload.jti
+invalidJtiClaimCondition = typeof jwtPayload.jti != "string"
+// We advise to limit expiry time to now + 5 minutes. Allowing an extra 10 seconds to mitigate edge cases:
+expClaimTooLongCondition = expExpiry > 310
+
+// Set the error message to the first error condition that returns true
+context.setVariable('InvalidJwt.ErrorMessage',
   missingKidCondition && missingKidMessage
+  || missingOrInvalidTypCondition && missingOrInvalidTypMessage
+  || missingExpClaimCondition && missingExpClaimMessage
+  || invalidExpiryTimeCondition && invalidExpiryTimeMessage
+  || missingOrInvalidIssClaimCondition && missingOrInvalidIssClaimMessage
+  || missingJtiClaimCondition && missingJtiClaimMessage
+  || invalidJtiClaimCondition && invalidJtiMessage
+  || expClaimTooLongCondition && expClaimTooLongMessage
   || noErrorMessage
 )
-
-
-context.setVariable('InvalidJwt.MissingOrInvalidTypHeader', typeof jwtHeaders.typ != "string" || jwtHeaders.typ.toLowerCase() != "jwt");
-context.setVariable('InvalidJwt.MissingExpClaim', !jwtPayload.exp);
-context.setVariable('InvalidJwt.InvalidExpiryTime', typeof jwtPayload.exp != "number");
-context.setVariable('InvalidJwt.MissingOrInvalidIssClaim', !jwtPayload.iss || jwtPayload.iss != jwtPayload.sub);
-context.setVariable('InvalidJwt.MissingJtiClaim', !jwtPayload.jti);
-context.setVariable('InvalidJwt.InvalidJtiClaim', typeof jwtPayload.jti != "string");
-
-// We advise to limit expiry time to now + 5 minutes. Allowing an extra 10 seconds to mitigate edge cases:
-seconds_remaining = context.getVariable("jwt.DecodeJWT.FromClientAssertionFormParam.seconds_remaining")
-context.setVariable('InvalidJwt.ExpClaimTooLong', seconds_remaining > 310);
