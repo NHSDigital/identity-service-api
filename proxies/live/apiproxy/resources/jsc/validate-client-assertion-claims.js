74c2b29ccda7611186fe0fb44a7eb8df81635ecc
Ed Feldman Bright <ed.feldmanbright@nhs.net>
2022-10-27 11:12:35 +0000

    Readd missing alg check

--- proxies/live/apiproxy/resources/jsc/validate-client-assertion-claims.js
+++ proxies/live/apiproxy/resources/jsc/validate-client-assertion-claims.js
@@ -22,6 +22,7 @@ missingOrInvalidIssClaimMessage = "Missing or non-matching iss/sub claims in JWT
 missingJtiClaimMessage = "Missing jti claim in JWT"
 invalidJtiMessage = "Jti claim must be a unique string value such as a GUID"
 expClaimTooLongMessage = "Invalid exp claim in JWT - more than 5 minutes in future"
+missingAlgHeaderMessage = "Missing 'alg' header in JWT"
 invalidAlgHeaderMessage = "Invalid 'alg' header in JWT - unsupported JWT algorithm - must be 'RS512'"
 jwtExpiredMessage = "Invalid exp claim in JWT - JWT has expired"
 jtiExistsInCacheMessage = "Non-unique jti claim in JWT"
@@ -34,6 +35,7 @@ missingExpClaimCondition = !jwtPayload.exp
 invalidExpiryTimeCondition = typeof jwtPayload.exp != "number"
 missingOrInvalidIssClaimCondition = !jwtPayload.iss || jwtPayload.iss != jwtPayload.sub
 missingJtiClaimCondition = !jwtPayload.jti
+missingAlgHeaderCondition = !jwtHeaders.alg
 invalidJtiClaimCondition = typeof jwtPayload.jti != "string"
 invalidAlgHeaderCondition = jwtHeaders.alg != "RS512"
 jtiExistsInCacheCondition = cachedJtiValue == jwtPayload.jti
@@ -45,6 +47,7 @@ expClaimTooLongCondition = expExpiry > 310
 // Set the error message to the first error condition that returns true
 context.setVariable('InvalidJwt.ErrorMessage',
   invalidAlgHeaderCondition && invalidAlgHeaderMessage
+  || invalidAlgHeaderCondition && invalidAlgHeaderMessage
   || jwtExpiredCondition && jwtExpiredMessage
   || missingKidCondition && missingKidMessage
   || missingOrInvalidTypCondition && missingOrInvalidTypMessage
