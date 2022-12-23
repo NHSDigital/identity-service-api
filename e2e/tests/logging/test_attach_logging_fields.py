46288bf95377371e5ca4c3f4c88792aa5544c9db
acarriedev <acarrie.dev@gmail.com>
2022-12-23 11:49:58 +0000

    APM-3433 Refactor client_credentials logging test

--- e2e/tests/logging/test_attach_logging_fields.py
+++ e2e/tests/logging/test_attach_logging_fields.py
@@ -66,6 +66,53 @@ class TestAttachLoggingFields:
                     force_new_token=True
                 ),
             ),
+            # User-restricted NHS-login combined P5
+            pytest.param(
+                {
+                    "auth_type": "user",
+                    "auth_grant_type": "authorization_code",
+                    "auth_level": "p5",
+                    "auth_provider": "nhs-login",
+                    "auth_user_id": "9912003072"
+                },
+                marks=pytest.mark.nhsd_apim_authorization(
+                    access="patient",
+                    level="P5",
+                    login_form={"username": "9912003072"},
+                    force_new_token=True
+                ),
+            ),
+            # User-restricted NHS-login combined P9
+            pytest.param(
+                {
+                    "auth_type": "user",
+                    "auth_grant_type": "authorization_code",
+                    "auth_level": "p9",
+                    "auth_provider": "nhs-login",
+                    "auth_user_id": "9912003071"
+                },
+                marks=pytest.mark.nhsd_apim_authorization(
+                    access="patient",
+                    level="P9",
+                    login_form={"username": "9912003071"},
+                    force_new_token=True
+                ),
+            ),
+            # Application-restricted client_credentials
+            pytest.param(
+                {
+                    "auth_type": "app",
+                    "auth_grant_type": "client_credentials",
+                    "auth_level": "level3",
+                    "auth_provider": "apim",
+                    "auth_user_id": ""
+                },
+                marks=pytest.mark.nhsd_apim_authorization(
+                    access="application",
+                    level="level3",
+                    force_new_token=True
+                ),
+            ),
         ]
     )
     def test_access_token_fields_for_logging_cis2_combined(
@@ -80,99 +127,6 @@ class TestAttachLoggingFields:
 
         for attribute, _ in expected_token_attributes.items():
             assert token_attributes[attribute] == expected_token_attributes[attribute]
-
-#     @pytest.mark.simulated_auth
-#     @pytest.mark.happy_path
-#     @pytest.mark.logging
-#     @pytest.mark.parametrize('scope', ['P9', 'P5', 'P0'])
-#     async def test_access_token_fields_for_logging_when_using_authorization_code_nhs_login(self, scope, helper):
-#         # Given
-#         apigee_trace = ApigeeApiTraceDebug(proxy=f"canary-api-{ENVIRONMENT}")
-
-#         # Make authorize request to retrieve state2
-#         response = await self.oauth.hit_oauth_endpoint(
-#             method="GET",
-#             endpoint="authorize",
-#             params={
-#                 "client_id": self.oauth.client_id,
-#                 "redirect_uri": self.oauth.redirect_uri,
-#                 "response_type": "code",
-#                 "state": "1234567890",
-#                 "scope": "nhs-login"
-#             },
-#             allow_redirects=False,
-#         )
-
-#         state = helper.get_param_from_url(
-#             url=response["headers"]["Location"], param="state"
-#         )
-#         # Make simulated auth request to authenticate
-#         response = await self.oauth.hit_oauth_endpoint(
-#             base_uri=MOCK_IDP_BASE_URL,
-#             method="POST",
-#             endpoint="nhs_login_simulated_auth",
-#             params={
-#                 "response_type": "code",
-#                 "client_id": self.oauth.client_id,
-#                 "redirect_uri": self.oauth.redirect_uri,
-#                 "scope": "openid",
-#                 "state": state,
-#             },
-#             headers={"Content-Type": "application/x-www-form-urlencoded"},
-#             data={
-#                 "state": state,
-#                 "auth_method": scope
-#             },
-#             allow_redirects=False,
-#         )
-
-#         # Make initial callback request
-#         auth_code = helper.get_param_from_url(
-#             url=response["headers"]["Location"], param="code"
-#         )
-
-#         response = await self.oauth.hit_oauth_endpoint(
-#             method="GET",
-#             endpoint="callback",
-#             params={"code": auth_code, "client_id": "some-client-id", "state": state},
-#             allow_redirects=False,
-#         )
-
-#         auth_code = helper.get_param_from_url(
-#             url=response["headers"]["Location"], param="code"
-#         )
-
-#         response = await self.oauth.hit_oauth_endpoint(
-#             method="POST",
-#             endpoint="token",
-#             data={
-#                 "grant_type": "authorization_code",
-#                 "state": state,
-#                 "code": auth_code,
-#                 "redirect_uri": self.oauth.redirect_uri,
-#                 "client_id": self.oauth.client_id,
-#                 "client_secret": self.oauth.client_secret
-#             },
-#             allow_redirects=False,
-#         )
-
-#         access_token = response['body']['access_token']
-#         # When
-#         await apigee_trace.start_trace()
-#         requests.get(f"{CANARY_API_URL}", headers={"Authorization": f"Bearer {access_token}"})
-
-#         # Then
-#         auth_type = await apigee_trace.get_apigee_variable_from_trace(name='accesstoken.auth_type')
-#         auth_grant_type = await apigee_trace.get_apigee_variable_from_trace(name='accesstoken.auth_grant_type')
-#         auth_level = await apigee_trace.get_apigee_variable_from_trace(name='accesstoken.auth_level')
-#         auth_provider = await apigee_trace.get_apigee_variable_from_trace(name='accesstoken.auth_provider')
-#         auth_user_id = await apigee_trace.get_apigee_variable_from_trace(name='accesstoken.auth_user_id')
-
-#         assert auth_type == 'user'
-#         assert auth_grant_type == 'authorization_code'
-#         assert auth_level == scope.lower()
-#         assert auth_provider == 'apim-mock-nhs-login'
-#         assert auth_user_id == '9912003071'
 
 #     @pytest.mark.happy_path
 #     @pytest.mark.logging
