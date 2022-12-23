d1f8a24bfa14105f217d08f2046b94588784c1ce
acarriedev <acarrie.dev@gmail.com>
2022-12-23 11:24:34 +0000

    APM-3433 Refactor combined nhs-login P0

--- e2e/tests/logging/test_attach_logging_fields.py
+++ e2e/tests/logging/test_attach_logging_fields.py
@@ -31,26 +31,55 @@ def access_token_api():
 class TestAttachLoggingFields:
     @pytest.mark.happy_path
     @pytest.mark.logging
-    @pytest.mark.nhsd_apim_authorization(
-        access="healthcare_worker",
-        level="aal3",
-        login_form={"username": "656005750104"},
-        force_new_token=True
+    @pytest.mark.parametrize(
+        ("expected_token_attributes"),
+        [
+            # User-restricted CIS2 combined aal3
+            pytest.param(
+                {
+                    "auth_type": "user",
+                    "auth_grant_type": "authorization_code",
+                    "auth_level": "aal3",
+                    "auth_provider": "nhs-cis2",
+                    "auth_user_id": "656005750104"
+                },
+                marks=pytest.mark.nhsd_apim_authorization(
+                    access="healthcare_worker",
+                    level="aal3",
+                    login_form={"username": "656005750104"},
+                    force_new_token=True
+                ),
+            ),
+            # User-restricted NHS-login combined P0
+            pytest.param(
+                {
+                    "auth_type": "user",
+                    "auth_grant_type": "authorization_code",
+                    "auth_level": "p0",
+                    "auth_provider": "nhs-login",
+                    "auth_user_id": "9912003073"
+                },
+                marks=pytest.mark.nhsd_apim_authorization(
+                    access="patient",
+                    level="P0",
+                    login_form={"username": "9912003073"},
+                    force_new_token=True
+                ),
+            ),
+        ]
     )
     def test_access_token_fields_for_logging_cis2_combined(
         self,
         _nhsd_apim_auth_token_data,
-        access_token_api
+        access_token_api,
+        expected_token_attributes
     ):
         access_token = _nhsd_apim_auth_token_data["access_token"]
         token_data = access_token_api.get_token_details(access_token)
         token_attributes = get_token_details(token_data)
 
-        assert token_attributes["auth_type"] == "user"
-        assert token_attributes["auth_grant_type"] == "authorization_code"
-        assert token_attributes["auth_level"] == "aal3"
-        assert token_attributes["auth_provider"] == "nhs-cis2"
-        assert token_attributes["auth_user_id"] == "656005750104"
+        for attribute, _ in expected_token_attributes.items():
+            assert token_attributes[attribute] == expected_token_attributes[attribute]
 
 #     @pytest.mark.simulated_auth
 #     @pytest.mark.happy_path
