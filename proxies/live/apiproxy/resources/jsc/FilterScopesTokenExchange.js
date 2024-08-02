078b8ad5ea25da3c12167dd67ef14ee3636437e7
NautiyalVikas1 <157499123+NautiyalVikas1@users.noreply.github.com>
2024-08-02 10:23:09 +0100

    AAL2 Changes

--- proxies/live/apiproxy/resources/jsc/FilterScopesTokenExchange.js
+++ proxies/live/apiproxy/resources/jsc/FilterScopesTokenExchange.js
@@ -1,8 +1,9 @@
 var api_product_scopes = String(context.getVariable('original_scope'));
 var idTokenIssuer = context.getVariable('idTokenIssuer');
 if (idTokenIssuer == "nhs-cis2") {
-    var id_token_acr = context.getVariable('jwt.DecodeJWT.FromSubjectTokenFormParam.decoded.claim.acr');
-    id_token_acr = ':' + id_token_acr.slice(0, 4).toLowerCase() + ':';
+    var id_token_acr = context.getVariable('jwt.DecodeJWT.FromSubjectTokenFormParam.decoded.claim.authentication_assurance_level');
+   // id_token_acr = ':' + id_token_acr.slice(0, 4).toLowerCase() + ':';
+    id_token_acr = ':aal' + id_token_acr + ':';
 }
 else {
     var id_token_acr = context.getVariable('jwt.DecodeJWT.FromSubjectTokenFormParam.decoded.claim.identity_proofing_level');
