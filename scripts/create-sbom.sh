cdbb125196cbc96b11c12f86145b21bfd7fa03cf
Valswyn-NHS <valswyn.alfonso1@nhs.net>
2025-09-03 13:05:51 +0100

    Updated the create sbom script

--- scripts/create-sbom.sh
+++ scripts/create-sbom.sh
@@ -1,8 +1,9 @@
+#!/bin/bash
 REPO_ROOT=$(git rev-parse --show-toplevel)
 
 syft -o spdx-json . > sbom.json
 
 for tool in "$@"; do
-  echo "Creating SBOM for $tool and merging"
-  syft -q -o spdx-json "$(which "$tool")" | python "$REPO_ROOT/scripts/update-sbom.py"
+   echo "Creating SBOM for $tool and merging"
+   syft -q -o spdx-json "$(which "$tool")" | python "$REPO_ROOT/scripts/update-sbom.py"
 done
